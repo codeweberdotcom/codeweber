d0d761e963258497a5fa10458f0261c96aa7daa9
Denis Korablev <info@codeweber.com>
2022-08-18 11:47:27 +0300

    page_header v1

--- templates/sections/common-pageheader.php
+++ templates/sections/common-pageheader.php
@@ -1,11 +1,14 @@
-<div class="section-pageheader py-5 mb-0 has-img-background">
-
-	<img class="is-background" src="<?php the_post_thumbnail_url( 'brk_big' ); ?>" alt="<?php the_title(); ?>">
-
-	<div class="container my-5 py-5 text-center text-light">
-
-		<h1 class="display-4"><?php the_title(); ?></h1>
-
+<section class="wrapper bg-soft-primary">
+	<div class="container pt-10 pb-12 pt-md-14 pb-md-14 text-center">
+		<div class="row">
+			<div class="col-md-7 col-lg-6 col-xl-5 mx-auto">
+				<h1 class="display-1 mb-3"><?php brk_page_title(); ?></h1>
+				<p class="lead px-lg-5 px-xxl-8"></p>
+				<?php brk_breadcrumbs(); ?>
+			</div>
+			<!-- /column -->
+		</div>
+		<!-- /.row -->
 	</div>
-
-</div>
+	<!-- /.container -->
+</section>
