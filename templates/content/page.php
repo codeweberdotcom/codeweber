d18cbc460a730840a9a2b5390230484f7254dd4d
Denis Korablev <info@codeweber.com>
2022-08-23 15:08:42 +0300

    page pagination delete

--- templates/content/page.php
+++ templates/content/page.php
@@ -3,21 +3,9 @@ global $post;
 ?>
 <div class="container py-14 py-md-16" id="post-<?php the_ID(); ?>">
 	<div class=" row gx-lg-8 gx-xl-12">
-		<div class="col">
+		<div class="col-12">
 			<?php
-			the_content();
-
-			wp_link_pages(
-				array(
-					'before'        => '<nav class="nav"><span class="nav-link">' . esc_html__('Part:', 'codeweber') . '</span>',
-					'after'         => '</nav>',
-					'link_before'   => '<span class="nav-link">',
-					'link_after'    => '</span>',
-				)
-			);
-			?>
+			the_content(); ?>
 		</div>
-		<?php get_sidebar(); ?>
-		<!-- /column .sidebar -->
 	</div>
 </div> <!-- #post-<?php the_ID(); ?> -->
