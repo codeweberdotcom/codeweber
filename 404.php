7e9ae7ec29dee9c31373aed71fe6049095d6b3cc
Denis Korablev <info@codeweber.com>
2022-09-01 15:25:53 +0300

    404 page

--- 404.php
+++ 404.php
@@ -1,28 +1,24 @@
 <?php get_header(); ?>
 
-<main id="content-wrapper">
-
-	<div class="container">
-
-		<div class="row py-5 justify-content-center">
-
-			<div class="col-sm-8">
-
-				<h1 class="mb-5 text-center">
-					<?php esc_html_e( 'Oops, that page can&#39;t be found', 'bricks' ); ?>
-				</h1>
-
-				<h2 class="mb-5 text-center">
-					<?php esc_html_e( 'Error 404', 'bricks' ); ?>
-				</h2>
-
+<section class="wrapper bg-light">
+	<div class="container pt-12 pt-md-14 pb-14 pb-md-16">
+		<div class="row">
+			<div class="col-lg-9 col-xl-8 mx-auto">
+				<figure class="mb-10"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/dist/img/illustrations/404.png" srcset="<?php echo get_template_directory_uri(); ?>/dist/img/illustrations/404@2x.png 2x" alt=""></figure>
 			</div>
-
+			<!-- /column -->
+			<div class="col-lg-8 col-xl-7 col-xxl-6 mx-auto text-center">
+				<h1 class="mb-3"><?php esc_html_e('Oops! Page Not Found.', 'codeweber'); ?> </h1>
+				<p class="lead mb-7 px-md-12 px-lg-5 px-xl-7"><?php esc_html_e('The page you are looking for is not available or has been moved. Try a different page or go to homepage with the button below.', 'codeweber'); ?></p>
+				<a href="/" class="btn btn-primary rounded-pill"><?php esc_html_e('Go to Homepage', 'codeweber'); ?></a>
+			</div>
+			<!-- /column -->
 		</div>
-		
+		<!-- /.row -->
 	</div>
-
-</main> <!-- #content-wrapper -->
+	<!-- /.container -->
+</section>
+<!-- /section -->
 
 <?php
 get_footer();
