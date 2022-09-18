5d7fa0dcd30d41c0321fdbd5e94f75f8a8db4fd5
Denis Korablev <info@codeweber.com>
2022-09-18 12:27:25 +0300

    contact_100 v2

--- templates/flexible-content/contact/contact_100/contact_100.php
+++ templates/flexible-content/contact/contact_100/contact_100.php
@@ -1,104 +1,109 @@
 <section id="<?php echo esc_html($args['block_id']); ?>" class="wrapper bg-light d-flex flex-wrap <?php echo esc_html($args['block_class']); ?>">
 
-
    <div class="container py-0 order-2">
-      <?php $anchor_array = array(); ?>
-      <?php if (have_rows('adresses')) : ?>
-         <?php $s = 1; ?>
-         <?php while (have_rows('adresses')) : the_row(); ?>
-            <?php if (have_rows('address')) : ?>
-               <?php while (have_rows('address')) : the_row(); ?>
-                  <article id="<?php echo $s; ?>">
-                     <div class="container py-14 py-md-16 pb-md-0">
-                        <div class="row gx-lg-0 gy-10 ">
-                           <div class="col-lg-3">
-                              <h2 class="display-4 mb-3 text-dark"> <?php the_sub_field('town'); ?></h2>
-                              <?php array_push($anchor_array, get_sub_field('town')); ?>
-                           </div>
-                           <!--/column -->
-                           <div class="col-lg-8  offset-lg-1 grid">
-                              <div class="row gx-md-5 gy-5 align-items-center counter-wrapper isotope">
-                                 <?php $select_address = get_sub_field('select_address'); ?>
-                                 <?php if ($select_address) : ?>
-                                    <?php $i = 1; ?>
-                                    <?php foreach ($select_address as $post) : ?>
-                                       <?php setup_postdata($post); ?>
-                                       <div class="item col-12">
+      <?php $towns = get_sub_field('towns'); ?>
+      <?php if ($towns) : ?>
+         <?php $num = 1; ?>
+         <?php foreach ($towns as $term) : ?>
+            <article id="<?php echo $num; ?>">
+               <div class="container py-14 py-md-16 pb-md-0">
+                  <div class="row gx-lg-0 gy-10 ">
+                     <div class="col-lg-3">
+                        <h2 class="display-4 mb-3 text-dark">Офисы в городе <?php echo esc_html($term->name); ?>
+                        </h2>
+                     </div>
+                     <!--/column -->
+                     <?php $args = array(
+                        'post_type' => 'office_addresses',
+                        'orderby' => 'date',
+                        'order'   => 'DESC',
+                        'tax_query' => array(
+                           array(
+                              'taxonomy' => 'towns',
+                              'field'    => 'slug',
+                              'terms'    => $term->slug
+                           )
+                        )
+                     );
+                     $query = new WP_Query($args);
+                     if ($query->have_posts()) { ?>
+                        <div class="col-lg-8  offset-lg-1 grid">
+                           <div class="row gx-md-5 gy-5 align-items-center counter-wrapper isotope">
+                              <?php $addresse_num = 1; ?>
+                              <?php while ($query->have_posts()) {
+                                 $query->the_post();
+                                 $post_id = get_the_ID(); ?>
+                                 <div class="item col-12">
+                                    <?php if (have_rows('office_addresses', $post_id)) : ?>
+                                       <?php while (have_rows('office_addresses', $post_id)) : the_row(); ?>
                                           <div class="card shadow-lg lift">
                                              <div class="card-body">
                                                 <div class="d-flex d-lg-block d-xl-flex flex-row flex-wrap">
                                                    <div class="col-12">
-                                                      <?php if (have_rows('office_addresses', $post)) : ?>
-                                                         <?php while (have_rows('office_addresses', $post)) : the_row(); ?>
-                                                            <div class="row mb-3 wrapper-border pb-3">
-                                                               <div class="col-2 col-md-2 col-lg-2 col-xl-1">
-                                                                  <span class="icon btn btn-circle btn-primary pe-none me-5"><span class="number fs-18"><?php echo $i; ?></span></span>
-                                                               </div>
-                                                               <div class="col align-self-center">
-                                                                  <h3 class="mb-1"> <?php the_sub_field('city'); ?></h3>
-                                                               </div>
-                                                               <?php if (get_sub_field('map_link')) { ?>
-                                                                  <div class="col align-self-center d-flex justify-content-end">
-                                                                     <a href="<?php the_sub_field('map_link'); ?>" class="hover more">На карте</a>
-                                                                  </div>
-                                                               <?php
-                                                               }
-                                                               ?>
+                                                      <div class="row mb-3 wrapper-border pb-3">
+                                                         <div class="col-2 col-md-2 col-lg-2 col-xl-1">
+                                                            <span class="icon btn btn-circle btn-primary pe-none me-5"><span class="number fs-18"><?php echo $addresse_num; ?></span></span>
+                                                         </div>
+                                                         <div class="col align-self-center">
+                                                            <h3 class="mb-1"> <?php echo esc_html($term->name); ?></h3>
+                                                         </div>
+                                                         <?php if (get_sub_field('map_link')) { ?>
+                                                            <div class="col align-self-center d-flex justify-content-end">
+                                                               <a href="<?php the_sub_field('map_link'); ?>" class="hover more">На карте</a>
                                                             </div>
-                                                            <div class="row">
-                                                               <div class="col-10 col-md-6 offset-2 offset-xl-0 mb-3 mb-xl-0 col-xl-5">
-                                                                  <p class="mb-0 fs-15 text-primary">Адрес:</p>
-                                                                  <?php the_sub_field('address_1'); ?><br>
-                                                                  <?php the_sub_field('address_2'); ?>
-                                                               </div>
-                                                               <div class="col-10 col-md-4 offset-2 offset-md-0 mb-3 mb-xl-0 col-xl-3">
-                                                                  <?php if (have_rows('working_time')) : ?>
-                                                                     <p class="mb-0 fs-15 text-primary">Время работы:</p>
-                                                                     <?php while (have_rows('working_time')) : the_row(); ?>
-                                                                        <?php the_sub_field('working_time'); ?><br>
-                                                                     <?php endwhile; ?>
-                                                                  <?php endif; ?>
-                                                               </div>
-                                                               <div class="col-10 col-md-10 offset-2 offset-md-2 offset-xl-0 mb-3 mb-xl-0 col-xl-4">
-                                                                  <?php if (have_rows('phones_office')) : ?>
-                                                                     <p class="mb-0 fs-15 text-primary">Телефоны:</p>
-                                                                     <?php while (have_rows('phones_office')) : the_row(); ?>
-                                                                        <a href="tel:<?php the_sub_field('phone_office'); ?>"><?php the_sub_field('phone_office'); ?></a>
-                                                                        <br>
-                                                                     <?php endwhile; ?>
-                                                                  <?php endif; ?>
-                                                               </div>
-                                                            </div>
-                                                         <?php endwhile; ?>
-                                                      <?php endif; ?>
+                                                         <?php
+                                                         }
+                                                         ?>
+                                                      </div>
+                                                      <div class="row">
+                                                         <div class="col-10 col-md-6 offset-2 offset-xl-0 mb-3 mb-xl-0 col-xl-5">
+                                                            <p class="mb-0 fs-15 text-primary">Адрес:</p>
+                                                            <?php the_sub_field('address_1'); ?><br>
+                                                            <?php the_sub_field('address_2'); ?>
+                                                         </div>
+                                                         <div class="col-10 col-md-4 offset-2 offset-md-0 mb-3 mb-xl-0 col-xl-3">
+                                                            <?php if (have_rows('working_time')) : ?>
+                                                               <p class="mb-0 fs-15 text-primary">Время работы:</p>
+                                                               <?php while (have_rows('working_time')) : the_row(); ?>
+                                                                  <?php the_sub_field('working_time'); ?><br>
+                                                               <?php endwhile; ?>
+                                                            <?php endif; ?>
+                                                         </div>
+                                                         <div class="col-10 col-md-10 offset-2 offset-md-2 offset-xl-0 mb-3 mb-xl-0 col-xl-4">
+                                                            <?php if (have_rows('phones_office')) : ?>
+                                                               <p class="mb-0 fs-15 text-primary">Телефоны:</p>
+                                                               <?php while (have_rows('phones_office')) : the_row(); ?>
+                                                                  <a href="tel:<?php the_sub_field('phone_office'); ?>"><?php the_sub_field('phone_office'); ?></a>
+                                                                  <br>
+                                                               <?php endwhile; ?>
+                                                            <?php endif; ?>
+                                                         </div>
+                                                      </div>
                                                    </div>
                                                 </div>
                                              </div>
                                              <!--/.card-body -->
                                           </div>
                                           <!--/.card -->
-                                       </div>
-                                       <!--/column -->
-                                       <?php $i++; ?>
-                                    <?php endforeach; ?>
-                                    <?php wp_reset_postdata(); ?>
-                                 <?php endif; ?>
-                              </div>
-                              <!--/.row -->
+                                       <?php endwhile; ?>
+                                    <?php endif; ?>
+                                 </div>
+                                 <!--/column -->
+                                 <?php $addresse_num++; ?>
+                              <?php } ?>
                            </div>
-                           <!--/column -->
+                           <!--/.row -->
                         </div>
-                        <!--/.row -->
-                     </div>
-                     <!-- /.container -->
-                  </article>
-
-               <?php endwhile; ?>
-            <?php endif; ?>
-            <?php $s++; ?>
-         <?php endwhile; ?>
+                        <!--/column -->
+                     <?php  } ?>
+                  </div>
+               </div>
+            </article>
+            <?php $num++; ?>
+         <?php endforeach; ?>
       <?php endif; ?>
    </div>
+
 
    <div class="container py-0 order-1">
       <div class="row">
@@ -113,12 +118,14 @@
                            <div class="form-select-wrapper w-100">
                               <select class="form-select" aria-label="Default select example" onchange="location = this.value;">
                                  <option value="#0" selected>Нажмите для выбора города</option>
-                                 <?php $f = 1; ?>
-                                 <?php foreach ($anchor_array as $value) { ?>
-                                    <option value="#<?php echo $f; ?>"><?php echo $value; ?></option>
-                                    <?php $f++; ?>
-                                 <?php }
-                                 ?>
+                                 <?php $towns = get_sub_field('towns'); ?>
+                                 <?php if ($towns) : ?>
+                                    <?php $num_option = 1; ?>
+                                    <?php foreach ($towns as $term) : ?>
+                                       <option value="#<?php echo $num_option; ?>"><?php echo esc_html($term->name); ?></option>
+                                       <?php $num_option++; ?>
+                                    <?php endforeach; ?>
+                                 <?php endif; ?>
                               </select>
                            </div>
                         </span>
@@ -134,8 +141,5 @@
       <!-- /.row -->
    </div>
    <!-- /.container -->
-
-
-
 </section>
 <!-- /section -->
