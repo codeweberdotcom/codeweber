020fc895939daf4dd13757b4b1d5329f987a9cf2
Denis Korablev <info@codeweber.com>
2022-09-21 23:21:53 +0300

    socialicons icon size fs-30

--- templates/components/socialicons.php
+++ templates/components/socialicons.php
@@ -6,7 +6,7 @@ foreach (brk_socialicons() as $key => $value) {
 
 	if (get_field('social_' . $key, 'option')) { ?>
 		<a href="<?php echo esc_attr(get_field('social_' . $key, 'option')); ?>" title="<?php echo esc_attr($value['social-name']); ?>" target="_blank">
-			<i class="<?php echo esc_attr($value['icon-style']) . ' ' . esc_attr($value['icon-name']); ?>"></i>
+			<i class="fs-30 <?php echo esc_attr($value['icon-style']) . ' ' . esc_attr($value['icon-name']); ?>"></i>
 		</a>
 
 <?php
