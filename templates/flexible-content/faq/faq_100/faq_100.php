365ae80e285abb6f912d88cbe27de165519c9fc4
Inna <inna@z-website.ru>
2022-09-25 18:23:54 +0300

    faq 100 v1

--- templates/flexible-content/faq/faq_100/faq_100.php
+++ templates/flexible-content/faq/faq_100/faq_100.php
@@ -13,11 +13,7 @@ foreach ($terms as $term) : ?>
 <?php
    $args = array(
       'post_type' => 'faq',
-<<<<<<< HEAD
       'post_status' => 'publish',
-=======
-      'post_status'    => 'publish',
->>>>>>> 7d34dba1cafc504c7610bcf96e611fd48d9b2881
       'tax_query' => array(
          array(
             'taxonomy' => 'faq_categories',
