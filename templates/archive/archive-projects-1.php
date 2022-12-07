c84b34bebe914fb8ec50ec81bb1c02fce6abf764
Denis Korablev <info@codeweber.com>
2022-12-07 14:59:17 +0300

    Projects

---
+++ templates/archive/archive-projects-1.php
@@ -0,0 +1,87 @@
+<section class="section-frame">
+   <div class="wrapper bg-gray">
+      <div class="container py-13 py-md-17 text-center">
+         <div class="row">
+            <div class="col-lg-10 col-xxl-8 mx-auto">
+               <h1 class="display-1 mb-3"><?php codeweber_page_title(); ?></h1>
+               <?php brk_breadcrumbs(); ?>
+            </div>
+            <!-- /column -->
+         </div>
+         <!-- /.row -->
+      </div>
+      <!-- /.container -->
+   </div>
+   <!-- /.wrapper -->
+</section>
+<!-- /section -->
+<section class="wrapper bg-light">
+   <div class="container py-14 py-md-16">
+      <div class="row mt-6 mb-10 mb-lg-15">
+         <div class="col-xl-10 mx-auto">
+            <div class="projects-tiles">
+               <?php
+               while (have_posts()) :
+                  the_post(); ?>
+                  <div class="project grid grid-view">
+                     <div class="row g-6 isotope">
+                        <div class="item col-md-6">
+                           <div class="project-details d-flex justify-content-center flex-column">
+                              <div class="post-header">
+                                 <div class="post-category text-primary mb-3">
+                                    <?php echo wp_get_post_terms(get_the_ID(), 'projects_category')[0]->name;  ?>
+                                 </div>
+                                 <h2 class="post-title mb-3"><?php the_title(); ?></h2>
+                              </div>
+                              <!-- /.post-header -->
+                              <div class="post-content">
+                                 <p><?php the_field('short_description'); ?></p>
+                                 <a href="<?php the_permalink(); ?>" class="more hover link-primary"><?php esc_html_e('See Project', 'codeweber') ?></a>
+                              </div>
+                              <!-- /.post-content -->
+                           </div>
+                           <!-- /.project-details -->
+                        </div>
+                        <!-- /.item -->
+                        <?php if (have_rows('izobrazheniya')) : ?>
+                           <?php $num = 0; ?>
+                           <?php while (have_rows('izobrazheniya')) : the_row(); ?>
+                              <?php if (have_rows('image')) : ?>
+                                 <?php while (have_rows('image')) : the_row(); ?>
+                                    <?php if ($num <= 2) { ?>
+                                       <?php $image = get_sub_field('image'); ?>
+                                       <?php if ($image) : ?>
+                                          <div class="item col-md-6">
+                                             <figure class="itooltip itooltip-light hover-scale rounded" title='<?php if (get_sub_field('title')) { ?><h5 class="mb-0"><?php the_sub_field('title'); ?></h5><?php }; ?>'><a href="<?php echo esc_url($image['sizes']['brk_big']); ?>" data-glightbox="title: <?php the_sub_field('title'); ?>" data-gallery="<?php echo get_the_ID(); ?>"> <img src="<?php echo esc_url($image['sizes']['project_1_1']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" /></a>
+                                             </figure>
+                                          </div>
+                                          <!-- /.item -->
+                                       <?php endif; ?>
+                                    <?php } ?>
+                                 <?php endwhile; ?>
+                              <?php endif; ?>
+                              <?php $num++; ?>
+                           <?php endwhile; ?>
+                        <?php else : ?>
+                           <?php // no rows found 
+                           ?>
+                        <?php endif; ?>
+                     </div>
+                     <!-- /.row -->
+                  </div>
+                  <!-- /.project -->
+               <?php endwhile; ?>
+            </div>
+            <!-- /.projects-tiles -->
+         </div>
+         <!-- /column -->
+      </div>
+      <!-- /.row -->
+
+      <?php codeweber_pagination(); ?>
+      <!-- /post pagination -->
+
+   </div>
+   <!-- /.container -->
+</section>
+<!-- /section -->
---
+++ templates/archive/archive-projects-5.php
@@ -0,0 +1,64 @@
+      <section class="wrapper bg-light">
+         <div class="container pt-10 pt-md-14">
+            <div class="row">
+               <div class="col-md-8 col-lg-7 col-xl-6 col-xxl-5">
+                  <h1 class="display-1 mb-3"><?php codeweber_page_title(); ?></h1>
+                  <?php if (get_theme_mod('project_description')) { ?>
+                     <p class="lead fs-lg pe-lg-15 pe-xxl-12"><?php echo get_theme_mod('project_description'); ?></p>
+                  <?php } else { ?>
+                     <p class="lead fs-lg pe-lg-15 pe-xxl-12">Check out some of our awesome projects with creative ideas and great design.</p>
+                  <?php }; ?>
+                  <?php brk_breadcrumbs(); ?>
+               </div>
+               <!-- /column -->
+            </div>
+            <!-- /.row -->
+         </div>
+         <!-- /.container -->
+      </section>
+      <!-- /section -->
+
+      <?php $terms = get_terms([
+         'taxonomy' => 'projects_category',
+         'hide_empty' => true,
+      ]);
+      ?>
+
+      <section class="wrapper bg-light">
+         <div class="container py-14 py-md-16">
+            <div class="grid grid-view projects-masonry pb-14 pb-md-16">
+               <div class="row gx-md-10 gy-10 gy-md-13 isotope">
+                  <?php
+                  global $query_string;
+                  query_posts($query_string . "&posts_per_page=9");
+                  if (
+                     have_posts()
+                  ) :
+                     while (have_posts('showposts=5')) {
+                        the_post();
+                        $taxonomy_list = wp_get_post_terms($post->ID, 'projects_category', array('fields' => 'names')); ?>
+                        <?php $taxonomy_list_slug = wp_get_post_terms($post->ID, 'projects_category', array('fields' => 'slugs')); ?>
+                        <div class="project item col-md-6 col-xl-4 <?php echo implode(' ', $taxonomy_list_slug); ?>">
+                           <figure class="lift rounded mb-6"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'archive_4'); ?></a></figure>
+                           <div class="project-details d-flex justify-content-center flex-column">
+                              <div class="post-header">
+                                 <div class="post-category text-line mb-3 text-purple"><?php echo implode(', ', $taxonomy_list); ?></div>
+                                 <h3 class="post-title"><?php echo esc_html($post->post_title); ?></h3>
+                              </div>
+                              <!-- /.post-header -->
+                           </div>
+                           <!-- /.project-details -->
+                        </div>
+                        <!-- /.project -->
+                  <?php };
+                  endif; ?>
+               </div>
+               <!-- /.row -->
+            </div>
+            <!-- /.grid -->
+            <?php codeweber_pagination(); ?>
+            <!-- /post pagination -->
+         </div>
+         <!-- /.container -->
+      </section>
+      <!-- /section -->
