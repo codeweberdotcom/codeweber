2213cbe81c5b6c0c6b8751bfe7595f6dd32c9dcc
Denis Korablev <info@codeweber.com>
2023-01-27 01:14:26 +0300

    Hero banner 22

--- templates/flexible-content/hero-banner/hero_banner_22/hero_banner_22.php
+++ templates/flexible-content/hero-banner/hero_banner_22/hero_banner_22.php
@@ -26,52 +26,6 @@ $block = new CW_Settings(
       'divider' => 'true',
       //'divider_angles' => 'upper-start',
       'divider_wave' => '<div class="overflow-hidden"><div class="divider text-white mx-n2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60"><path fill="currentColor" d="M0,0V60H1440V0A5771,5771,0,0,1,0,0Z" /></svg></div></div><!-- /.overflow-hidden -->',
-
-
-
-      //'image_pattern' => '<figure class="rounded shadow-lg"><img src="%1$s" srcset="%2$s" alt="%3$s""></figure>',
-      //'image_link' => '/dist/img/photos/about13.jpg',
-      //'image_thumb_size' => 'sandbox_hero_3',
-      //'image_big_size' => 'project_1',
-
-
-      // 'swiper' => array(
-      //    'swiper_container_class' => 'rounded overflow-hidden',
-      //    'image_class' => '',
-      //    'wrapper_image_class' => '',
-      //    'image_pattern' => '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>',
-      //    'image_thumb_size' => 'sandbox_hero_14',
-      //    'image_demo' => '<figure class="rounded"><img src="' . get_template_directory_uri() . '/dist/img/photos/about5.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about5@2x.jpg 2x" alt="" /></figure>',
-      //    'image_big_size' => 'project_1',
-      //    'img_link' => '/dist/img/photos/about5.jpg',
-      //    'data_margin' => '30',
-      //    'nav' => 'false',
-      //    'nav_color' => 'nav-light',
-      //    'nav_position' => 'nav-start',
-      //    'dots' => 'true',
-      //    'dots_color' => 'nav-start',
-      //    'dots_position' => 'dots-over',
-      //    'swiper_effect' => 'slide',
-      //    'base_items' => '1',
-      //    'items_xs' => '1',
-      //    'items_sm' => '1',
-      //    'items_md' => '1',
-      //    'items_lg' => '1',
-      //    'items_xl' => '1',
-      //    'items_xxl' => '1',
-      //    'autoplay' => 'true',
-      //    'autoplay_time' => '3000',
-      //    'loop' => 'loop',
-      //    'autoheight' => 'false',
-      //    'image_shape' => 'rounded',
-
-      // ),
-
-      // 'features' => '<div class="col-6 col-lg-3"><h3 class="counter counter-lg text-white">7518</h3><p>Completed Projects</p></div><!--/column -->',
-      // 'features_pattern' => '<div class="col-6 col-lg-3 %1$s"><h3 class="counter counter-lg text-white">%3$s</h3><p>%4$s</p></div><!--/column -->',
-
-      //'column_class_1' => '',
-      //'column_class_2' => 'offset-lg-1  order-lg-2',
    )
 );
 ?>
