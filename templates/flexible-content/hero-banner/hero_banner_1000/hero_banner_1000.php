d25221e7b24443de28e4a30c362a68a87eb8f0ae
Denis Korablev <info@codeweber.com>
2023-01-26 11:28:17 +0300

    hero banner 13 stage 1

--- templates/flexible-content/hero-banner/hero_banner_1000/hero_banner_1000.php
+++ templates/flexible-content/hero-banner/hero_banner_1000/hero_banner_1000.php
@@ -13,75 +13,69 @@ $block = new CW_Settings(
     'buttons' => '<a href="#" class="btn btn-lg btn-gradient gradient-1 rounded">Explore Now</a>',
     'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
 
-    'background_class_default' => 'wrapper bg-light',
-    'background_data_default' => '/dist/img/photos/bg16.png',
-    'background_video_preview' => '/dist/img/photos/movie2.jpg',
-    'background_video_url' => '/dist/media/movie2.mp4',
-    'background_pattern_url' => '/dist/img/pattern.png',
-
-    //'divider' => true,
-    //'divider_angles' => 'upper-start',
-    //'divider_wave' => '<!-- Wave 2 --><div class="overflow-hidden"><div class="divider text-white mx-n2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60"><path fill="currentColor" d="M0,0V60H1440V0A5771,5771,0,0,1,0,0Z"/></svg></div></div><!-- /.overflow-hidden -->',
-
-    // 'image_pattern' => '<figure %5s><img class="w-auto" src="%1$s" srcset="%2$s" alt="%3$s" /></figure>',
-    // 'image_link' => '/dist/img/illustrations/i6.png',
-    // 'image_thumb_size' => 'sandbox_hero_1',
-    // 'image_big_size' => 'project_1',
-    //'shapes' => array('<div class="shape bg-dot primary rellax w-17 h-21" data-rellax-speed="1" style="top: -2.5rem; right: -2.7rem;"></div>'),
-
-    'swiper' => array(
-      'swiper_container_class' => 'img-fluid position-relative',
-      'image_class' => 'img-fluid position-relative',
-      'wrapper_image_class' => 'img-fluid position-relative',
-      'image_pattern' => '<figure %5s><img %4$s src="%1$s" srcset="%2$s" alt="%3$s" /></figure>',
-      'image_thumb_size' => 'sandbox_hero_18',
-      'image_demo' => '<figure><img class="img-fluid " src="' . get_template_directory_uri() . '/dist/img/illustrations/3d6.png" srcset="' . get_template_directory_uri() . '/dist/img/illustrations/3d6@2x.png 2x" alt="" data-cue="fadeIn" data-delay="300" /></figure>',
-      'image_big_size' => 'project_1',
-      'img_link' => '/dist/img/illustrations/3d6.png',
-      'data_margin' => '30',
-      'nav' => 'false',
-      'nav_color' => NULL,
-      'nav_position' => NULL,
-      'dots' => 'false',
-      'dots_color' => NULL,
-      'dots_position' => NULL,
-      'swiper_effect' => 'fade',
-      'base_items' => '1',
-      'items_xs' => '1',
-      'items_sm' => '1',
-      'items_md' => '1',
-      'items_lg' => '1',
-      'items_xl' => '1',
-      'items_xxl' => '1',
-      'autoplay' => 'true',
-      'autoplay_time' => '3000',
-      'loop' => 'loop',
-      'autoheight' => 'false',
-      'image_shape' => '',
-      'label_demo' => '<div class="card shadow-lg" style="bottom: 5rem; right: 5rem;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
-
-      'label_pattern' => '<div class="card shadow-lg position-absolute zindex-1 %6$s" %7$s><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div>%2$s</div><div><h3 class="counter mb-0 text-nowrap">%3$s</h3><p class="fs-14 lh-sm mb-0 text-nowrap">%4$s</p>%5$s</div></div></div><!--/.card-body --></div><!--/.card -->',
-    ),
-
-    'column_class_1' => 'order-lg-2 offset-lg-1',
-    'column_class_2' => '',
-
   )
 );
 ?>
 
 
 
-<div class="container position-relative">
-  <div class="row">
-    <div class="col-6">
-      <?php echo $block->swiper_final; ?>
+<div class="swiper-container swiper-hero dots-over" data-margin="0" data-autoplay="true" data-autoplaytime="5000" data-nav="true" data-dots="true" data-items="1">
+  <div class="swiper">
+    <div class="swiper-wrapper">
+      <?php if (have_rows('cw_slider_full')) : ?>
+        <?php while (have_rows('cw_slider_full')) : the_row();
+          $block = new CW_Settings(
+            $cw_settings = array(
+              'title' => 'We bring solutions to make life easier.',
+              'patternTitle' => '<h2 class="display-1 fs-56 mb-4 text-white animate__animated animate__slideInDown animate__delay-1s">%s</h2>',
 
-      <?php
-      // $labels = new CW_Labels($pattern, $demo, NULL);
-      // echo $labels->final_labels;
-      ?>
+              'paragraph' => 'We are a creative company that focuses on long term relationships with customers.',
+              'patternParagraph' => '<p class="lead fs-23 lh-sm mb-7 text-white animate__animated animate__slideInRight animate__delay-2s">%s</p>',
+
+              'buttons' => '<div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#" class="btn btn-lg btn-outline-white rounded-pill">Read More</a></div>',
+              'buttons_pattern' => '<div class="animate__animated animate__slideInUp animate__delay-3s">%s</div>',
+            )
+          );
+          if (have_rows('cw_image1')) {
+            while (have_rows('cw_image1')) {
+              the_row();
+              $cw_image = get_sub_field('cw_image');
+              if ($cw_image) {
+                $image_link = esc_url($cw_image['url']);
+                $image_alt = esc_url($cw_image['alt']);
+              }
+            }
+          } ?>
+          <div class="swiper-slide h-100 bg-overlay bg-overlay-400 bg-dark" style="background-image:url(<?php echo $image_link; ?>">
+            <div class="container h-100">
+              <div class="row h-100">
+                <div class="col-md-10 offset-md-1 col-lg-7 offset-lg-0 col-xl-6 col-xxl-5 text-center text-lg-start justify-content-center align-self-center align-items-start">
+                  <?php echo $block->title; ?>
+                  <!-- title -->
+                  <?php echo $block->paragraph; ?>
+                  <!--/pargraph -->
+                  <?php echo $block->buttons; ?>
+                  <!--/buttons group -->
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!--/.container -->
+          </div>
+          <!--/.swiper-slide -->
+
+
+        <?php endwhile; ?>
+      <?php else : ?>
+        <?php // no rows found 
+        ?>
+      <?php endif; ?>
+
 
     </div>
+    <!--/.swiper-wrapper -->
   </div>
+  <!-- /.swiper -->
 </div>
+<!-- /.swiper-container -->
