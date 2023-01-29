3d83e3125a801c41ceef5c6dbd69ca0297787499
Denis Korablev <info@codeweber.com>
2023-01-29 15:57:21 +0300

    process 2 ++

--- templates/flexible-content/process/process_2/process_2.php
+++ templates/flexible-content/process/process_2/process_2.php
@@ -11,21 +11,9 @@ $block = new CW_Settings(
       'paragraph' => 'We make your spending <span class="underline">stress-free</span> for you to have the perfect control.',
       'patternParagraph' => '<p class="lead fs-lg mb-8">%s</p>',
 
-      // 'subtitle' => 'Grow Your Business with Our Solutions.',
-      // 'patternSubtitle' => '<h2 class="fs-15 text-uppercase text-muted mb-3">%s</h2>',
-
-      'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
-      'buttons' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900"><span><a href="#" class="btn btn-lg btn-primary rounded-pill me-2">Explore Now</a></span><span><a href="#" class="btn btn-lg btn-outline-primary rounded-pill">Free Trial</a></span></div>',
-
       'background_class_default' => 'wrapper bg-light',
-      //'background_data_default' => '/dist/img/photos/bg16.png',
-      //'background_video_preview' => '/dist/img/photos/movie2.jpg',
-      //'background_video_url' => '/dist/media/movie2.mp4',
-      //'background_pattern_url' => '/dist/img/pattern.png',
 
       'divider' => 'true',
-      //'divider_angles' => 'upper-start',
-      //'divider_wave' => '<!-- Wave 2 --><div class="overflow-hidden"><div class="divider text-white mx-n2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60"><path fill="currentColor" d="M0,0V60H1440V0A5771,5771,0,0,1,0,0Z"/></svg></div></div><!-- /.overflow-hidden -->',
 
       'shapes' => array(
          '<div class="shape bg-dot primary rellax w-17 h-21" data-rellax-speed="1" style="top: -2rem; left: -1.9rem;"></div>',
@@ -36,11 +24,10 @@ $block = new CW_Settings(
 
       'label_pattern' => '<div class="card shadow-lg position-absolute zindex-1 %6$s" %7$s><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div>%2$s</div><div><h3 class="counter mb-0 text-nowrap">%3$s</h3><p class="fs-14 lh-sm mb-0 text-nowrap">%4$s</p>%5$s</div></div></div><!--/.card-body --></div><!--/.card -->',
 
-      'features' => '<div class="col-md-6 col-lg-3"> <span class="icon btn btn-circle btn-lg btn-primary pe-none mb-4"><span class="number">02</span></span><h4 class="mb-1">Prepare</h4><p class="mb-0">Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis.</p></div><!--/column -->',
+      'features' => '<div class="col-md-6 col-lg-3"><span class="icon btn btn-circle btn-lg btn-primary pe-none mb-4"><span class="number">02</span></span><h4 class="mb-1">Prepare</h4><p class="mb-0">Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis.</p></div><!--/column -->',
 
-      'features_pattern' => '<div class="col-md-6 col-lg-3 %1$s">%2$s<h4 class="mb-1">%3$s</h4><p class="mb-0">%4$s</p></div><!--/column -->',
+      'features_pattern' => '<div class="col-md-6 col-lg-3 %1$s">%2$s<h4 class="mb-1">%3$s</h4><p class="mb-3">%4$s</p>%5$s</div><!--/column -->',
       'features_style_icon' => 'mb-4',
-
    )
 );
 ?>
