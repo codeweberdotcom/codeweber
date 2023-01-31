b2b2a46c1292ded6f8a0063172f8506744cf8d4c
Denis Korablev <info@codeweber.com>
2023-02-01 02:16:53 +0300

    Class Accordeon - Custom

--- templates/flexible-content/hero-banner/hero_banner_1000/hero_banner_1000.php
+++ templates/flexible-content/hero-banner/hero_banner_1000/hero_banner_1000.php
@@ -1,42 +1,16 @@
 <?php
-$array = array(
-   array(get_template_directory_uri() . '/dist/img/photos/about3.jpg', 'sandbox_hero_11', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="rounded shadow"><img src="' . get_template_directory_uri() . '/dist/img/photos/about13.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about3@2x.jpg 2x" alt=""></figure>'),
-   array(get_template_directory_uri() . '/dist/img/photos/about2.jpg', 'sandbox_hero_11', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="rounded shadow"><img src="' . get_template_directory_uri() . '/dist/img/photos/about2.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about3@2x.jpg 2x" alt=""></figure>'),
-   array(get_template_directory_uri() . '/dist/img/photos/about3.jpg', 'sandbox_hero_11', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="rounded shadow"><img src="' . get_template_directory_uri() . '/dist/img/photos/about3.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about3@2x.jpg 2x" alt=""></figure>'),
-   array(get_template_directory_uri() . '/dist/img/photos/about4.jpg', 'sandbox_hero_11', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="rounded shadow"><img src="' . get_template_directory_uri() . '/dist/img/photos/about4.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about3@2x.jpg 2x" alt=""></figure>'),
-   array(get_template_directory_uri() . '/dist/img/photos/about5.jpg', 'sandbox_hero_11', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="rounded shadow"><img src="' . get_template_directory_uri() . '/dist/img/photos/about5.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about3@2x.jpg 2x" alt=""></figure>'),
-);
 
-$ttt = new CW_MultiImage($array);
+$accordeon = new CW_Accordeon(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+?>
 
+<section class="wrapper bg-gradient-primary">
+   <div class="container pt-10 pt-md-14 pb-8 ">
+      <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
 
-if (isset($ttt->final_images_array[0])) {
-   echo $ttt->final_images_array[0];
-}
+         <?php
+         echo $accordeon->accordeon_custom;
+         ?>
 
-
-if (isset($ttt->final_images_array[1])) {
-   echo $ttt->final_images_array[1];
-}
-
-
-if (isset($ttt->final_images_array[2])) {
-   echo $ttt->final_images_array[2];
-}
-
-if (isset($ttt->final_images_array[3])) {
-   echo $ttt->final_images_array[3];
-}
-
-if (isset($ttt->final_images_array[4])) {
-   echo $ttt->final_images_array[4];
-}
-
-
-if (isset($ttt->final_images_array[5])) {
-   echo $ttt->final_images_array[5];
-}
-
-if (isset($ttt->final_images_array[6])) {
-   echo $ttt->final_images_array[6];
-}
+      </div>
+   </div>
+</section>
