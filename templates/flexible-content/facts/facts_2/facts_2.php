dba7ca4759a521ecf875ec15f1791004db781865
Inna <inna@z-website.ru>
2023-02-04 01:01:43 +0300

    Facts 1++, 2, 3, 4

--- templates/flexible-content/facts/facts_2/facts_2.php
+++ templates/flexible-content/facts/facts_2/facts_2.php
@@ -1,63 +1,47 @@
 <?php
-/* Add settings */
-$settings = new Settings();
-// адрес корня темы , обязательная переменная для демо
-$settings->title = 'We are proud of our works'; // демо заголовок
-$settings->paragraph = 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur.'; // демо параграф
-$settings->subtitle = 'Company Facts'; // демо подзаголовок
-// $settings->imageurl = get_template_directory_uri() . '/dist/img/photos/bg11.jpg'; // демо фото
-// $settings->backgroundurl = $settings->root_theme . '/dist/img/photos/bg37.jpg';
-// $settings->videourl = get_template_directory_uri() . '/dist/media/movie.mp4'; // демо видео
-// $settings->typewriter = 'customer satisfaction,business needs,creative ideas'; // демо данные эффекта печатной машинки
-$settings->backgroundcolor = 'dark'; // цвет бэкгрануда темной темы
-//$settings->backgroundcolor_light = 'gray'; // если есть другой цвет бэкграунда, например soft-primary или gray
-$settings->textcolor = 'light'; // цвет текста темной темы
-$settings->section_id = esc_html($args['block_id']); // присваиваем секции уникальный id
-$settings->GetDataACF(); // получаем занчения полей ACF
 
+/**
+ * Facts 2
+ */
+$block = new CW_Settings(
+   $cw_settings = array(
+      'title' => 'We are proud of our works',
+      'patternTitle' => '<h2 class="display-4 pe-xl-15">%s</h2>',
 
-/* Facts */
-$facts = new Faq;
-$facts->text_color = $settings->textcolor;
-$facts->icon_classes = 'icon btn btn-circle btn-lg disabled mb-3';
-$facts->icon_svg_classes = 'svg-inject icon-svg icon-svg-lg  mb-3';
-$facts->pattern = '<div class="col-md-4">
-                  %3$s
-                  <h3 class="counter">%1$s</h3>
-                  <p class="mb-0">%2$s</p>
-                  </div>
-                  <!--/column -->';
-$facts->default_template = '<div class="col-md-4">
-                  <img src="' . $settings->root_theme . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-lg text-primary mb-3" alt="" />
-                  <h3 class="counter">7518</h3>
-                  <p class="mb-0">Completed Projects</p>
-               </div>
-               <!--/column -->
-               <div class="col-md-4">
-                  <img src="' . $settings->root_theme . '/dist/img/icons/lineal/user.svg" class="svg-inject icon-svg icon-svg-lg text-primary mb-3" alt="" />
-                  <h3 class="counter">3472</h3>
-                  <p class="mb-0">Happy Customers</p>
-               </div>
-               <!--/column -->
-               <div class="col-md-4">
-                  <img src="' . $settings->root_theme . '/dist/img/icons/lineal/briefcase-2.svg" class="svg-inject icon-svg icon-svg-lg text-primary mb-3" alt="" />
-                  <h3 class="counter">2184</h3>
-                  <p class="mb-0">Expert Employees</p>
-               </div>
-               <!--/column -->';
+      // 'paragraph' => 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+      // 'patternParagraph' => '<p class="mb-0">%s</p>',
+
+      'subtitle' => 'Company Facts',
+      'patternSubtitle' => '<p class="fs-15 text-uppercase text-primary mb-3">%s</p>',
+
+      'background_class_default' => 'wrapper bg-light',
+
+      'divider' => true,
+
+      'column_class_1' => '',
+      'column_class_2' => 'order-lg-2',
+
+      'features' => '<div class="col-md-4"><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-lg text-primary mb-3" alt="" /><h3 class="counter">7518</h3><p class="mb-0">Completed Projects</p></div><!--/column -->',
+      'features_pattern' => '<div class="col-md-4">%2$s<h3 class="counter">%3$s</h3><p class="mb-0">%4$s</p></div><!--/column -->',
+      'features_style_icon' => 'mb-3'
+   )
+);
 ?>
 
-<section class="wrapper bg-light">
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
    <div class="container py-14 py-md-16">
       <div class="row">
-         <div class="col-lg-4">
-            <h2 class="fs-15 text-uppercase text-primary mb-3"><?php echo $settings->subtitle; ?></h2>
-            <h3 class="display-4 pe-xl-15"><?php echo $settings->title; ?></h3>
+         <div class="col-lg-4 <?php echo $block->column_class_1; ?>">
+            <?php echo $block->subtitle; ?>
+            <!--/subtitle -->
+            <?php echo $block->title; ?>
+            <!--/title -->
          </div>
          <!-- /column -->
-         <div class="col-lg-8">
+         <div class="col-lg-8 <?php echo $block->column_class_2; ?>">
             <div class="row align-items-center counter-wrapper gy-6 text-center">
-               <?php echo $facts->Facts_3(); ?>
+               <?php echo $block->features; ?>
+               <!--/features -->
             </div>
             <!--/.row -->
          </div>
@@ -66,5 +50,9 @@ $facts->default_template = '<div class="col-md-4">
       <!-- /.row -->
    </div>
    <!-- /.container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
