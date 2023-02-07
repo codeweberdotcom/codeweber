8bcf4951a6b4de6f7295f9f422335caccd7b7e43
Inna <inna@z-website.ru>
2023-02-07 13:32:19 +0300

    Call to Action all

--- templates/flexible-content/call-to-action/call-to-action_3/call-to-action_3.php
+++ templates/flexible-content/call-to-action/call-to-action_3/call-to-action_3.php
@@ -1,16 +1,59 @@
-<section class="wrapper bg-light">
+<?php
+
+/**
+ *  Call to Action 3
+ */
+
+//Icon
+$final_icon = '<img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/puzzle-2.svg" class="svg-inject icon-svg icon-svg-md mb-4" alt="" />';
+$icon = new CW_Icon(NULL, NULL, 'mb-4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, $final_icon);
+
+$block = new CW_Settings(
+   $cw_settings = array(
+      // 'subtitle' => 'FAQ',
+      // 'patternSubtitle' => '<div class="fs-15 text-uppercase text-primary mb-3">%s</div>',
+
+      'title' => 'Join Our Community',
+      'patternTitle' => '<h2 class="display-4 mb-3">%s</h2>',
+
+      'paragraph' => 'We are trusted by over 5000+ clients. Join them by using our services and grow your business.',
+      'patternParagraph' => '<p class="lead fs-lg mb-6 px-xl-10 px-xxl-15">%s</p>',
+
+      'buttons' => '<a href="#" class="btn btn-primary rounded">Join Us</a>',
+      'buttons_pattern' => '<div class="d-flex justify-content-center" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
+
+      'background_class_default' => 'wrapper bg-light',
+      // 'background_data_default' => '/dist/img/photos/bg3.jpg',
+      //'background_video_preview' => '/dist/img/photos/movie2.jpg',
+      //'background_video_url' => '/dist/media/movie2.mp4',
+      //'background_pattern_url' => '/dist/img/pattern.png',
+
+      'divider' => true,
+
+   )
+);
+?>
+
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
    <div class="container py-14 py-md-16 text-center">
       <div class="row">
          <div class="col-md-9 col-lg-7 col-xl-7 mx-auto text-center">
-            <img src="./assets/img/icons/lineal/puzzle-2.svg" class="svg-inject icon-svg icon-svg-md mb-4" alt="" />
-            <h2 class="display-4 mb-3">Join Our Community</h2>
-            <p class="lead fs-lg mb-6 px-xl-10 px-xxl-15">We are trusted by over 5000+ clients. Join them by using our services and grow your business.</p>
-            <a href="#" class="btn btn-primary rounded">Join Us</a>
+            <?php echo $icon->final_icon; ?>
+            <?php echo $block->title; ?>
+            <!--/title -->
+            <?php echo $block->paragraph; ?>
+            <!--/paragraph -->
+            <?php echo $block->buttons; ?>
+            <!--/buttons group -->
          </div>
          <!-- /column -->
       </div>
       <!-- /.row -->
    </div>
    <!-- /.container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
--- templates/flexible-content/call-to-action/call-to-action_4/call-to-action_4.php
+++ templates/flexible-content/call-to-action/call-to-action_4/call-to-action_4.php
@@ -1,18 +1,52 @@
+<?php
+
+/**
+ *  Call to Action 4
+ */
+
+
+$block = new CW_Settings(
+   $cw_settings = array(
+      // 'subtitle' => 'FAQ',
+      // 'patternSubtitle' => '<div class="fs-15 text-uppercase text-primary mb-3">%s</div>',
+
+      'title' => 'Join Our Community',
+      'patternTitle' => '<h2 class="fs-16 text-uppercase text-white mb-3">%s</h2>',
+
+      'paragraph' => 'We are <span class="underline-3 style-2 yellow">trusted</span> by over 5000+ clients. Join them now and grow your business.',
+      'patternParagraph' => '<p class="display-3 mb-8 px-lg-8 text-white">%s</p>',
+
+      'buttons' => '<div class="d-flex justify-content-center"><span><a class="btn btn-white rounded">Get Started</a></span></div>',
+      'buttons_pattern' => '<div class="d-flex justify-content-center" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
+
+      'background_class_default' => 'card image-wrapper bg-full bg-image bg-overlay bg-overlay-300 mb-14',
+      'background_data_default' => '/dist/img/photos/bg16.png',
+      //'background_video_preview' => '/dist/img/photos/movie2.jpg',
+      //'background_video_url' => '/dist/media/movie2.mp4',
+      //'background_pattern_url' => '/dist/img/pattern.png',
+
+      // 'divider' => true,
+
+   )
+);
+?>
+
 <section class="wrapper bg-light">
    <div class="container pb-13 pb-md-15">
-      <div class="card image-wrapper bg-full bg-image bg-overlay bg-overlay-300 mb-14" data-image-src="./assets/img/photos/bg16.png">
+      <div id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
          <div class="card-body p-10 p-xl-12">
             <div class="row text-center">
                <div class="col-xl-11 col-xxl-9 mx-auto">
-                  <h2 class="fs-16 text-uppercase text-white mb-3">Join Our Community</h2>
-                  <h3 class="display-3 mb-8 px-lg-8 text-white">We are <span class="underline-3 style-2 yellow">trusted</span> by over 5000+ clients. Join them now and grow your business.</h3>
+                  <?php echo $block->title; ?>
+                  <!--/title -->
+                  <?php echo $block->paragraph; ?>
+                  <!--/paragraph -->
                </div>
                <!-- /column -->
             </div>
             <!-- /.row -->
-            <div class="d-flex justify-content-center">
-               <span><a class="btn btn-white rounded">Get Started</a></span>
-            </div>
+            <?php echo $block->buttons; ?>
+            <!--/buttons group -->
          </div>
          <!--/.card-body -->
       </div>
