c32000e56243330de36e0c7fd1ded7887ee68de1
Inna <inna@z-website.ru>
2023-02-09 16:56:31 +0300

    Features 2, 3, 5, 7, 8

--- templates/flexible-content/features/features_2/features_2.php
+++ templates/flexible-content/features/features_2/features_2.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * CW
+ * Features 2
  */
-$features = new CW_Settings(
+$block = new CW_Settings(
    $cw_settings = array(
       'title' => 'The service we offer is specifically designed to meet your needs.',
       'patternTitle' => '<h3 class="display-4 mb-9 %2$s">%1$s</h3>',
@@ -13,11 +13,10 @@ $features = new CW_Settings(
       'patternSubtitle' => '<h2 class="fs-15 text-uppercase text-muted mb-3 %2$s">%1$s</h2>',
       // 'buttons' => '<span><a class="btn btn-primary rounded-pill me-2">Try It For Free</a></span>',
       // 'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
+
       'background_class_default' => 'wrapper bg-light',
-      //'background_data_default' => '/dist/img/photos/bg16.png',
-      //'background_video_preview' => '/dist/img/photos/movie2.jpg',
-      //'background_video_url' => '/dist/media/movie2.mp4',
-      //'background_pattern_url' => '/dist/img/pattern.png',
+
+      'divider' => true,
       // 'image_pattern' => '<figure %5$s title="dsdsds"><img class="w-auto" src="%1$s" srcset="%2$s" alt="%3$s" /></figure>',
       // 'image_link' => '/dist/img/illustrations/i2.png',
       // 'image_thumb_size' => 'sandbox_hero_1',
@@ -31,32 +30,28 @@ $features = new CW_Settings(
 );
 ?>
 
-<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $features->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $features->background_data; ?>>
-   <?php if ($features->background_video_bool == true) { ?>
-      <video poster="<?php echo $features->background_video_preview; ?>" src="<?php echo $features->background_video_url; ?>" autoplay loop playsinline muted></video>
-      <div class="video-content">
-      <?php } ?>
-      <!-- /video background -->
-      <div class="container py-14 py-md-16">
-         <div class="row">
-            <div class="col-lg-8 col-xl-7 col-xxl-6">
-               <?php echo $features->subtitle; ?>
-               <?php echo $features->title; ?>
-            </div>
-            <!-- /column -->
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
+   <div class="container py-14 py-md-16">
+      <div class="row">
+         <div class="col-lg-8 col-xl-7 col-xxl-6">
+            <?php echo $block->subtitle; ?>
+            <!--/subtitle -->
+            <?php echo $block->title; ?>
+            <!--/title -->
          </div>
-         <!-- /.row -->
-         <div class="row gx-md-8 gy-8">
-            <?php echo $features->features;
-            ?>
-         </div>
-         <!--/.row -->
+         <!-- /column -->
       </div>
-      <!-- /.container -->
-      <?php if ($features->background_video_bool == true) { ?>
+      <!-- /row -->
+      <div class="row gx-md-8 gy-8">
+         <?php echo $block->features; ?>
+         <!--/features -->
       </div>
-      </video>
-   <?php } ?>
-   <!-- /video background -->
+      <!--/row -->
+   </div>
+   <!-- /container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
