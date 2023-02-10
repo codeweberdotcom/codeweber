c9bfc92feaa56c588d00c03dd1a8a8e633780b80
Inna <inna@z-website.ru>
2023-02-10 09:28:18 +0300

    Features 9, 10

--- templates/flexible-content/features/features_9/features_9.php
+++ templates/flexible-content/features/features_9/features_9.php
@@ -1,4 +1,53 @@
-<section class="wrapper bg-light">
+<?php
+
+/**
+ * Features 9
+ */
+
+
+$block = new CW_Settings(
+   $cw_settings = array(
+
+      'title' => 'We have considered our solutions to support every stage of growth.',
+      'patternTitle' => '<h2 class="display-4 mb-5">%s</h2>',
+
+      'paragraph' => 'Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo.',
+      'patternParagraph' => '<p class="mb-5">%s</p>',
+
+      // 'subtitle' => 'We are a digital and branding company that believes in the power of creative strategy and along with great design.',
+      // 'patternSubtitle' => '<p class="lead fs-lg">%s</p>',
+
+      'background_class_default' => 'wrapper bg-light',
+      // 'background_data_default' => '/dist/img/map.png',
+      //'background_video_preview' => '/dist/img/photos/movie2.jpg',
+      //'background_video_url' => '/dist/media/movie2.mp4',
+      //'background_pattern_url' => '/dist/img/pattern.png',
+
+      'divider' => true,
+
+
+      'multi_image' => array(
+         array('/dist/img/photos/sa5.jpg', 'sandbox_about_4', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="img-fluid rounded shadow-lg mb-5"><img src="' . get_template_directory_uri() . '/dist/img/photos/sa5.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/sa5@2x.jpg 2x" alt=""></figure>'),
+
+         array('/dist/img/photos/sa6.jpg', 'sandbox_about_4', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="img-fluid rounded shadow-lg d-flex col-10 ms-auto"><img src="' . get_template_directory_uri() . '/dist/img/photos/sa6.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/sa6@2x.jpg 2x" alt=""></figure>'),
+
+         array('/dist/img/photos/sa7.jpg', 'sandbox_about_4', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="img-fluid rounded shadow-lg my-5"><img src="' . get_template_directory_uri() . '/dist/img/photos/sa7.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/sa7@2x.jpg 2x" alt=""></figure>'),
+
+         array('/dist/img/photos/sa8.jpg', 'sandbox_about_4', 'project_1', '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>', '<figure class="img-fluid rounded shadow-lg d-flex col-10"><img src="' . get_template_directory_uri() . '/dist/img/photos/sa8.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/sa8@2x.jpg 2x" alt=""></figure>'),
+      ),
+
+      'features' => '<div class="col-md-6 col-lg-4"><div class="d-flex flex-row"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/target.svg" class="svg-inject icon-svg icon-svg-sm text-aqua me-4" alt="" /></div><div><h4 class="mb-1">Fitness Goal</h4><p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, eget lacinia odio sem aget elit nullam quis risus eget.</p></div></div></div><!--/column -->',
+      'features_pattern' => '<div class="col-md-6 col-lg-4 %1$s"><div class="d-flex flex-row"><div>%2$s</div><div><h4>%3$s</h4><p class="mb-2">%4$s</p>%5$s</div></div></div><!--/column -->',
+      'features_style_icon' => 'me-4',
+
+      'column_class_1' => '',
+      'column_class_2' => 'order-lg-2',
+
+   )
+);
+?>
+
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
    <div class="container py-14 py-md-16">
       <div class="row gx-lg-0 gy-10 align-items-center">
          <div class="col-lg-6">
@@ -64,5 +113,9 @@
       <!-- /.row -->
    </div>
    <!-- /.container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
