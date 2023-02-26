62e7f827f1e1b4c4823e0a8a765af00c26af6aa3
Inna <inna@z-website.ru>
2023-02-26 10:59:03 +0300

    Contact all

--- templates/flexible-content/contact/contact_4/contact_4.php
+++ templates/flexible-content/contact/contact_4/contact_4.php
@@ -1,20 +1,78 @@
-<section class="wrapper bg-light">
+<?php
+
+/**
+ * Contact 4
+ */
+
+$address_line_1 = 'Moonshine St. 14/05 Light City,';
+$address_line_2 = 'London, United Kingdom';
+$e_mail_address = 'info@codeweber.com';
+$phone_number = '00 (123) 456 78 90';
+
+if (get_field('address_1', 'option')) {
+   $address_line_1 = get_field('address_1', 'option');
+}
+if (get_field('address_2', 'option')) {
+   $address_line_2 = get_field('address_2', 'option');
+}
+if (get_field('email', 'option')) {
+   $e_mail_address = get_field('email', 'option');
+}
+if (get_field('phone', 'option')) {
+   $phone_number = get_field('phone', 'option');
+}
+
+
+$block = new CW_Settings(
+   $cw_settings = array(
+      'title' => 'Get In Touch',
+      'patternTitle' => '<h2 class="fs-15 text-uppercase text-line text-primary text-center mb-3">%s</h2>',
+
+      'subtitle' => 'Got any questions? Don\'t hesitate to get in touch.',
+      'patternSubtitle' => '<h3 class="display-5 mb-7">%s</h3>',
+
+      'background_class_default' => 'wrapper bg-light',
+      'divider' => true,
+
+      'swiper' => array(
+         'swiper_container_class' => 'overflow-hidden',
+         'image_class' => 'w-auto',
+         'data_thumbs' => NULL,
+         'wrapper_image_class' => '',
+         'image_pattern' => '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>',
+         'image_thumb_size' => 'sandbox_faq_1',
+         'image_demo' => '<figure><img class="w-auto" src="' . get_template_directory_uri() . '/dist/img/illustrations/i5.png" srcset="' . get_template_directory_uri() . '/dist/img/illustrations/i5@2x.png 2x" alt="" /></figure>',
+         'image_big_size' => 'sandbox_hero_6',
+         'img_link' => '/dist/img/illustrations/i5.png',
+
+      ),
+
+      'column_class_1' => '',
+      'column_class_2' => 'order-lg-2',
+   )
+);
+?>
+
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
    <div class="container py-14 py-md-16">
       <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
-         <div class="col-lg-7">
-            <figure><img class="w-auto" src="./assets/img/illustrations/i5.png" srcset="./assets/img/illustrations/i5@2x.png 2x" alt="" /></figure>
+         <div class="col-lg-7 <?php echo $block->column_class_1; ?>">
+            <?php echo $block->swiper_final; ?>
+            <!--/swiper -->
          </div>
          <!--/column -->
-         <div class="col-lg-5">
-            <h2 class="fs-15 text-uppercase text-line text-primary text-center mb-3">Get In Touch</h2>
-            <h3 class="display-5 mb-7">Got any questions? Don't hesitate to get in touch.</h3>
+         <div class="col-lg-5 <?php echo $block->column_class_2; ?>">
+            <?php echo $block->title; ?>
+            <!--/title -->
+            <?php echo $block->subtitle; ?>
+            <!--/subtitle -->
             <div class="d-flex flex-row">
                <div>
                   <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-location-pin-alt"></i> </div>
                </div>
                <div>
-                  <h5 class="mb-1">Address</h5>
-                  <address>Moonshine St. 14/05 Light City, London</address>
+                  <h5 class="mb-1"><?php esc_html_e('Address', 'codeweber'); ?></h5>
+                  <address><?php echo $address_line_1; ?>, <?php echo $address_line_2; ?></address>
                </div>
             </div>
             <div class="d-flex flex-row">
@@ -22,8 +80,8 @@
                   <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-phone-volume"></i> </div>
                </div>
                <div>
-                  <h5 class="mb-1">Phone</h5>
-                  <p>00 (123) 456 78 90</p>
+                  <h5 class="mb-1"><?php esc_html_e('Phone', 'codeweber'); ?></h5>
+                  <p><?php echo $phone_number; ?></p>
                </div>
             </div>
             <div class="d-flex flex-row">
@@ -31,8 +89,8 @@
                   <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-envelope"></i> </div>
                </div>
                <div>
-                  <h5 class="mb-1">E-mail</h5>
-                  <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-body">sandbox@email.com</a></p>
+                  <h5 class="mb-1"><?php esc_html_e('E-mail', 'codeweber'); ?></h5>
+                  <p class="mb-0"><a href="mailto:<?php echo $e_mail_address; ?>" class="link-body"><?php echo $e_mail_address; ?></a></p>
                </div>
             </div>
          </div>
@@ -41,5 +99,9 @@
       <!--/.row -->
    </div>
    <!-- /.container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
--- templates/flexible-content/contact/contact_6/contact_6.php
+++ templates/flexible-content/contact/contact_6/contact_6.php
@@ -1,4 +1,51 @@
-<section class="wrapper bg-light">
+<?php
+
+/**
+ * Contact 6
+ */
+
+$address_line_1 = 'Moonshine St. 14/05 Light City,';
+$address_line_2 = 'London, United Kingdom';
+$e_mail_address = 'info@codeweber.com';
+$e_mail_address_1 = '';
+$phone_number = '00 (123) 456 78 90';
+$phone_number_1 = '';
+$map_url = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25387.23478654725!2d-122.06115399490332!3d37.309248660190086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb4571bd377ab%3A0x394d3fe1a3e178b4!2sCupertino%2C%20CA%2C%20USA!5e0!3m2!1sen!2str!4v1645437305701!5m2!1sen!2str';
+
+if (get_field('address_1', 'option')) {
+   $address_line_1 = get_field('address_1', 'option');
+}
+if (get_field('address_2', 'option')) {
+   $address_line_2 = get_field('address_2', 'option');
+}
+if (get_field('email', 'option')) {
+   $e_mail_address = get_field('email', 'option');
+}
+if (get_field('email_1', 'option')) {
+   $e_mail_address_1 = get_field('email_1', 'option');
+}
+if (get_field('phone', 'option')) {
+   $phone_number = get_field('phone', 'option');
+}
+if (get_field('phone_1', 'option')) {
+   $phone_number_1 = get_field('phone_1', 'option');
+}
+if (get_field('map_url', 'option')) {
+   $map_url = get_field('map_url', 'option');
+}
+
+
+$block = new CW_Settings(
+   $cw_settings = array(
+
+      'background_class_default' => 'wrapper bg-light',
+      'divider' => true,
+
+   )
+);
+?>
+
+<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
    <div class="container py-14 py-md-16">
       <div class="row">
          <div class="col-xl-10 mx-auto">
@@ -6,7 +53,7 @@
                <div class="row gx-0">
                   <div class="col-lg-6 align-self-stretch">
                      <div class="map map-full rounded-top rounded-lg-start">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25387.23478654725!2d-122.06115399490332!3d37.309248660190086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb4571bd377ab%3A0x394d3fe1a3e178b4!2sCupertino%2C%20CA%2C%20USA!5e0!3m2!1sen!2str!4v1645437305701!5m2!1sen!2str" style="width:100%; height: 100%; border:0" allowfullscreen></iframe>
+                        <iframe src="<?php echo $map_url; ?>" style="width:100%; height: 100%; border:0" allowfullscreen></iframe>
                      </div>
                      <!-- /.map -->
                   </div>
@@ -18,8 +65,8 @@
                               <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-location-pin-alt"></i> </div>
                            </div>
                            <div class="align-self-start justify-content-start">
-                              <h5 class="mb-1">Address</h5>
-                              <address>Moonshine St. 14/05 Light City, London, United Kingdom</address>
+                              <h5 class="mb-1"><?php esc_html_e('Address', 'codeweber'); ?></h5>
+                              <address><?php echo $address_line_1; ?>, <?php echo $address_line_2; ?></address>
                            </div>
                         </div>
                         <!--/div -->
@@ -28,8 +75,8 @@
                               <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-phone-volume"></i> </div>
                            </div>
                            <div>
-                              <h5 class="mb-1">Phone</h5>
-                              <p>00 (123) 456 78 90 <br class="d-none d-md-block" />00 (987) 654 32 10</p>
+                              <h5 class="mb-1"><?php esc_html_e('Phone', 'codeweber'); ?></h5>
+                              <p><?php echo $phone_number; ?> <?php if ($phone_number_1) { ?><br class="d-none d-md-block" /><?php echo $phone_number_1; ?><?php } ?></p>
                            </div>
                         </div>
                         <!--/div -->
@@ -38,9 +85,11 @@
                               <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-envelope"></i> </div>
                            </div>
                            <div>
-                              <h5 class="mb-1">E-mail</h5>
-                              <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-body">sandbox@email.com</a></p>
-                              <p class="mb-0"><a href="mailto:help@sandbox.com" class="link-body">help@sandbox.com</a></p>
+                              <h5 class="mb-1"><?php esc_html_e('E-mail', 'codeweber'); ?></h5>
+                              <p class="mb-0"><a href="mailto:<?php echo $e_mail_address; ?>" class="link-body"><?php echo $e_mail_address; ?></a></p>
+                              <?php if ($e_mail_address_1) { ?>
+                                 <p class="mb-0"><a href="mailto:<?php echo $e_mail_address_1; ?>" class="link-body"><?php echo $e_mail_address_1; ?></a></p>
+                              <?php } ?>
                            </div>
                         </div>
                         <!--/div -->
@@ -58,5 +107,9 @@
       <!-- /.row -->
    </div>
    <!-- /.container -->
+   <?php if ($block->divider_wave) {
+      echo $block->divider_wave;
+   } ?>
+   <!-- /divider -->
 </section>
 <!-- /section -->
