e64ad2f9c791610188ed9cd39f5c8bbbd28066ac
Denis Korablev <info@codeweber.com>
2023-03-03 21:27:51 +0300

    hero banner 3-1

--- templates/flexible-content/hero-banner/hero_banner_3_1/hero_banner_3_1.php
+++ templates/flexible-content/hero-banner/hero_banner_3_1/hero_banner_3_1.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Hero 16
+ * Hero 3_1
  */
 $block = new CW_Settings(
   $cw_settings = array(
@@ -11,8 +11,6 @@ $block = new CW_Settings(
     'paragraph' => 'Hello! I\'m Julia, a freelance user interface designer & developer based in London. I’m very passionate about the work that I do.',
     'patternParagraph' => '<p class="lead fs-25 lh-sm mb-7 px-md-10 px-lg-0">%s</p>',
 
-    // 'subtitle' => 'Grow Your Business with Our Solutions.',
-    // 'patternSubtitle' => '<h2 class="fs-15 text-uppercase text-muted mb-3">%s</h2>',
 
     'buttons' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">
           <span><a href="#" class="btn btn-lg btn-primary rounded-pill me-2">See My Works</a></span>
@@ -21,20 +19,8 @@ $block = new CW_Settings(
     'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
 
     'background_class_default' => 'wrapper bg-gray',
-    'background_data_default' => '/dist/img/photos/bg16.png',
-    'background_video_preview' => '/dist/img/photos/movie2.jpg',
-    'background_video_url' => '/dist/media/movie2.mp4',
-    'background_pattern_url' => '/dist/img/pattern.png',
 
     'divider' => true,
-    //'divider_angles' => 'upper-start',
-    //'divider_wave' => '<!-- Wave 2 --><div class="overflow-hidden"><div class="divider text-white mx-n2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 60"><path fill="currentColor" d="M0,0V60H1440V0A5771,5771,0,0,1,0,0Z"/></svg></div></div><!-- /.overflow-hidden -->',
-
-    // 'image_pattern' => '<figure %5$s><img class="w-auto" src="%1$s" srcset="%2$s" alt="%3$s" /></figure>',
-    // 'image_link' => '/dist/img/illustrations/i6.png',
-    // 'image_thumb_size' => 'sandbox_hero_1',
-    // 'image_big_size' => 'project_1',
-    //'shapes' => array('<div class="shape bg-dot primary rellax w-17 h-21" data-rellax-speed="1" style="top: -2.5rem; right: -2.7rem;"></div>'),
 
     'swiper' => array(
       'swiper_container_class' => 'w-100',
@@ -71,6 +57,10 @@ $block = new CW_Settings(
 
     'label_pattern' => '<div class="card shadow-lg position-absolute zindex-1 %6$s" %7$s><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div>%2$s</div><div><h3 class="counter mb-0 text-nowrap">%3$s</h3><p class="fs-14 lh-sm mb-0 text-nowrap">%4$s</p>%5$s</div></div></div><!--/.card-body --></div><!--/.card -->',
 
+    'features' => '<div class="col-12"><div class="d-flex flex-row"><div><div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5"> <i class="uil uil-phone-volume"></i> </div></div><div><h4 class="mb-1">24/7 Support</h4><p class="mb-0">Nulla vitae elit libero pharetra augue dapibus.</p></div></div></div><!--/column -->',
+    'features_pattern' => '<div class="col-12 %1$s"><div class="d-flex flex-row"><div>%2$s</div><div><h4 class="mb-1">%3$s</h4><p class="mb-0">%4$s</p>%5$s</div></div></div><!--/column -->',
+    'features_style_icon' => 'disabled me-5',
+
     'column_class_1' => '',
     'column_class_2' => 'order-lg-2 offset-lg-1',
 
@@ -89,8 +79,14 @@ $block = new CW_Settings(
       <div class="col-lg-5 text-center text-lg-start <?php echo $block->column_class_2; ?>" data-cues="slideInDown" data-group="page-title" data-delay="600">
         <?php echo $block->title; ?>
         <!--/title -->
-        <?php echo $block->paragraph; ?>
+        <?php // echo $block->paragraph; 
+        ?>
         <!--/pargraph -->
+        <div class="row gx-xl-10 gy-6 mb-6">
+          <?php echo $block->features; ?>
+          <!--/features -->
+        </div>
+        <!--/.row -->
         <?php echo $block->buttons; ?>
         <!--/buttons group -->
       </div>
