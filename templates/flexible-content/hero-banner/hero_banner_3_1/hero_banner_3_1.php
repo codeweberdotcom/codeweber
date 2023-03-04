fb02acf6787fece48d784b63f8371ea0bdb6f031
Inna <inna@z-website.ru>
2023-03-04 12:29:55 +0300

    Hero 3_1

--- templates/flexible-content/hero-banner/hero_banner_3_1/hero_banner_3_1.php
+++ templates/flexible-content/hero-banner/hero_banner_3_1/hero_banner_3_1.php
@@ -12,36 +12,33 @@ $block = new CW_Settings(
     'patternParagraph' => '<p class="lead fs-25 lh-sm mb-7 px-md-10 px-lg-0">%s</p>',
 
 
-    'buttons_pattern' => '<div class="d-flex flex-wrap justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
-    'buttons' => '<div><a class="btn btn-lg btn-primary rounded">Get Started</a></div>',
+    'buttons' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">
+          <span><a href="#" class="btn btn-lg btn-primary rounded-pill me-2">See My Works</a></span>
+          <span><a href="#" class="btn btn-lg btn-outline-primary rounded-pill">Contact Me</a></span>
+        </div>',
+    'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
 
-    'background_class_default' => 'wrapper bg-dark',
+    'background_class_default' => 'wrapper bg-gray',
 
     'divider' => true,
-    'divider_angles' => 'angled lower-start',
-
-
-    'features' => '<div class="col-12"><div class="d-flex flex-row"><div><div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5"> <i class="uil uil-phone-volume"></i> </div></div><div><h4 class="mb-1">24/7 Support</h4><p class="mb-0">Nulla vitae elit libero pharetra augue dapibus.</p></div></div></div><!--/column -->',
-    'features_pattern' => '<div class="col-12 %1$s"><div class="d-flex flex-row"><div>%2$s</div><div><h4 class="mb-1%9$s">%3$s</h4><p class="mb-0%10$s">%4$s</p>%5$s</div></div></div><!--/column -->',
-    'features_style_icon' => 'disabled me-5',
 
     'swiper' => array(
-      'swiper_container_class' => 'rounded',
-      'image_class' => 'rounded',
-      'wrapper_image_class' => 'rounded',
+      'swiper_container_class' => 'w-100',
+      'image_class' => 'w-auto',
+      'wrapper_image_class' => '',
       'image_pattern' => '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>',
       'image_thumb_size' => 'sandbox_hero_3',
-      'image_demo' => '<figure class="rounded shadow-lg"><img  src="' . get_template_directory_uri() . '/dist/img/photos/about13.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about13@2x.jpg 2x" alt="" /></figure>',
+      'image_demo' => '<div class="img-mask mask-1"><img src="' . get_template_directory_uri() . '/dist/img/photos/about17.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about17@2x.jpg 2x" alt="" /></div><div class="card shadow-lg position-absolute" style="bottom: 10%; right: 2%;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
       'image_big_size' => 'project_1',
-      'img_link' => '/dist/img/photos/about13.jpg',
+      'img_link' => '/dist/img/photos/about17.jpg',
       'data_margin' => '30',
       'nav' => 'true',
-      'nav_color' => 'nav-light',
-      'nav_position' => 'nav-start',
+      'nav_color' => NULL,
+      'nav_position' => NULL,
       'dots' => 'false',
-      'dots_color' => 'nav-start',
-      'dots_position' => 'dots-over',
-      'swiper_effect' => 'slide',
+      'dots_color' => NULL,
+      'dots_position' => NULL,
+      'swiper_effect' => NULL,
       'base_items' => '1',
       'items_xs' => '1',
       'items_sm' => '1',
@@ -49,58 +46,54 @@ $block = new CW_Settings(
       'items_lg' => '1',
       'items_xl' => '1',
       'items_xxl' => '1',
-      'autoplay' => 'true',
+      'autoplay' => 'false',
       'autoplay_time' => '3000',
-      'loop' => 'loop',
+      'loop' => 'false',
       'autoheight' => 'false',
-      'image_shape' => 'rounded'
+      'image_shape' => 'img-mask mask-1',
     ),
 
-    'label_demo' => '<div class="card shadow-lg position-absolute zindex-1" style="bottom: 2rem; right: 2rem;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
+    'label_demo' => '<div class="card shadow-lg" style="bottom: 5rem; right: 5rem;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
+
     'label_pattern' => '<div class="card shadow-lg position-absolute zindex-1 %6$s" %7$s><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div>%2$s</div><div><h3 class="counter mb-0 text-nowrap">%3$s</h3><p class="fs-14 lh-sm mb-0 text-nowrap">%4$s</p>%5$s</div></div></div><!--/.card-body --></div><!--/.card -->',
 
+    'features' => '<div class="col-12"><div class="d-flex flex-row"><div><div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5"> <i class="uil uil-phone-volume"></i> </div></div><div><h4 class="mb-1">24/7 Support</h4><p class="mb-0">Nulla vitae elit libero pharetra augue dapibus.</p></div></div></div><!--/column -->',
+    'features_pattern' => '<div class="col-12 %1$s"><div class="d-flex flex-row"><div>%2$s</div><div><h4 class="mb-1%9$s">%3$s</h4><p class="mb-0%10$s">%4$s</p>%5$s</div></div></div><!--/column -->',
+    'features_style_icon' => 'disabled me-5',
+
     'column_class_1' => '',
-    'column_class_2' => 'offset-lg-1  order-lg-2',
+    'column_class_2' => 'order-lg-2 offset-lg-1',
   )
 );
 ?>
 
 <section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
-  <?php if ($block->background_video_bool == true) { ?>
-    <video poster="<?php echo $block->background_video_preview; ?>" src="<?php echo $block->background_video_url; ?>" autoplay loop playsinline muted></video>
-    <div class="video-content">
-    <?php } ?>
-    <!-- /video background -->
-    <div class="container pt-7 pt-md-11 pb-8">
-      <div class="row gx-0 gy-10 align-items-center">
-        <div class="col-lg-6 <?php echo $block->column_class_1; ?>" data-cues="slideInDown" data-group="page-title" data-delay="600">
-          <?php echo $block->title; ?>
-          <!--/title -->
-          <div class="row gx-xl-10 gy-6 mb-6">
-            <?php echo $block->features; ?>
-            <!--/features -->
-          </div>
-          <?php echo $block->buttons; ?>
-          <!--/buttons group -->
-        </div>
-        <!-- /column -->
-        <div class="col-lg-5  mb-n18 <?php echo $block->column_class_2; ?>" data-cues="slideInDown">
-          <div class="position-relative">
-            <?php echo $block->swiper_final; ?>
-            <!--/swiper -->
-          </div>
-          <!-- /div -->
-        </div>
-        <!-- /column -->
+  <div class="container pt-12 pt-md-14 pb-14 pb-md-16">
+    <div class="row gy-10 gy-md-13 gy-lg-0 align-items-center">
+      <div class="col-md-8 col-lg-5 d-flex position-relative mx-auto <?php echo $block->column_class_1; ?>" data-cues="slideInDown" data-group="header">
+        <?php echo $block->swiper_final; ?>
+        <!--/swiper -->
       </div>
-      <!-- /.row -->
+      <!--/column -->
+      <div class="col-lg-5 text-center text-lg-start <?php echo $block->column_class_2; ?>" data-cues="slideInDown" data-group="page-title" data-delay="600">
+        <?php echo $block->title; ?>
+        <!--/title -->
+        <?php // echo $block->paragraph; 
+        ?>
+        <!--/pargraph -->
+        <div class="row gx-xl-10 gy-6 mb-6">
+          <?php echo $block->features; ?>
+          <!--/features -->
+        </div>
+        <!--/.row -->
+        <?php echo $block->buttons; ?>
+        <!--/buttons group -->
+      </div>
+      <!--/column -->
     </div>
-    <!-- /.container -->
-    <?php if ($block->background_video_bool == true) { ?>
-    </div>
-    </video>
-  <?php } ?>
-  <!-- /video background -->
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
   <?php if ($block->divider_wave) {
     echo $block->divider_wave;
   } ?>
