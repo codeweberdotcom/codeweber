b72f590a75ff89a48ccf758a212637f33ab797b9
Denis Korablev <info@codeweber.com>
2023-03-06 02:06:15 +0300

    woocommerce

--- templates/flexible-content/woocommerce/woocommerce_1/woocommerce_1.php
+++ templates/flexible-content/woocommerce/woocommerce_1/woocommerce_1.php
@@ -13,7 +13,7 @@ $block = new CW_Settings(
       'subtitle' => 'Check out some of our awesome projects with creative ideas and great design.',
       'patternSubtitle' => ' <h3 class="display-4 mb-9">%s</h3>',
 
-      'background_class_default' => 'wrapper bg-gray',
+      'background_class_default' => 'wrapper bg-light',
       'divider' => true,
 
       'shapes' => array('<div class="shape bg-line leaf rounded-circle rellax w-17 h-17" data-rellax-speed="1" style="top: -2rem; right: -0.6rem;"></div>', '<div class="shape bg-pale-violet rounded-circle rellax w-17 h-17" data-rellax-speed="1" style="bottom: -2rem; left: -0.4rem;"></div>'),
@@ -38,30 +38,28 @@ $block = new CW_Settings(
       <?php $products = get_sub_field('products');
       if ($products) { ?>
          <div class="swiper-container grid-view mb-6" data-margin="30" data-dots="true" data-items-xl="4" data-items-md="2" data-items-xs="1" data-nav="true">
-
             <div class="swiper">
                <div class="swiper-wrapper">
                   <?php
                   foreach ($products as $post_ids) { ?>
-
                      <div class="swiper-slide">
-                        <a href="<?php echo get_permalink($post_ids); ?>">
-                           <figure class="rounded mb-6">
-                              <?php
-                              $image = wp_get_attachment_image_src(get_post_thumbnail_id($post_ids), 'single-post-thumbnail');
-                              ?>
-                              <img src="<?php echo $image[0]; ?>" srcset="<?php echo $image[0]; ?>" alt="" />
-                              <a class="item-link" href="<?php echo $image[0]; ?>" data-glightbox="title: <?php echo get_the_title($post_ids); ?>;" data-gallery="projects-group"><i class="uil uil-focus-add"></i></a>
-                           </figure>
-                           <div class="project-details d-flex justify-content-center flex-column">
-                              <div class="post-header">
-                                 <h2 class="post-title h3"><a href="<?php echo get_permalink($post_ids); ?>" class="link-dark"><?php echo get_the_title($post_ids); ?></a></h2>
-                              </div>
-                              <!-- /.post-header -->
+                        <figure class="rounded mb-6">
+                           <?php
+                           $image = wp_get_attachment_image_src(get_post_thumbnail_id($post_ids), 'team-1');
+                           ?>
+                           <?php
+                           $image_big = wp_get_attachment_image_src(get_post_thumbnail_id($post_ids), 'sandbox_hero_18');
+                           ?>
+                           <img src="<?php echo $image[0]; ?>" srcset="<?php echo $image[0]; ?>" alt="" />
+                           <a class="item-link" href="<?php echo $image_big[0]; ?>" data-glightbox="title: <?php echo get_the_title($post_ids); ?>;" data-gallery="projects-group"><i class="uil uil-focus-add"></i></a>
+                        </figure>
+                        <div class="project-details d-flex justify-content-center flex-column">
+                           <div class="post-header">
+                              <h4 class="post-title h3"><a href="<?php echo get_permalink($post_ids); ?>" class="link-dark"><?php echo get_the_title($post_ids); ?></a></h4>
                            </div>
-                           <!-- /.project-details -->
-
-                        </a>
+                           <!-- /.post-header -->
+                        </div>
+                        <!-- /.project-details -->
                      </div>
                      <!--/.swiper-slide -->
                   <?php }
