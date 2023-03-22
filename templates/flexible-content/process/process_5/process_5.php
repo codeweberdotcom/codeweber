9588b189dd4d949b816dc0fa85a4429543595151
Denis Korablev <info@codeweber.com>
2023-03-22 12:24:00 +0300

    Subtitle Lead class fix

--- templates/flexible-content/process/process_5/process_5.php
+++ templates/flexible-content/process/process_5/process_5.php
@@ -16,11 +16,12 @@ $icon = new CW_Icon(NULL, NULL, 'mb-4', NULL, NULL, NULL, NULL, NULL, NULL, NULL
 
 $block = new CW_Settings(
    $cw_settings = array(
-      'title' => 'Find out everything you need to know and more about how we create our business process models.',
-      'patternTitle' => '<p class="lead fs-lg pe-lg-5">%s</p>',
+      'title' => 'How It Works?',
+      'patternTitle' => '<h3 class="display-6 mb-3">%s</h3>',
 
-      'subtitle' => 'How It Works?',
-      'patternSubtitle' => '<h2 class="display-6 mb-3">%s</h2>',
+      'subtitle' => 'Find out everything you need to know and more about how we create our business process models.',
+      'patternSubtitle' => '<p class="lead fs-lg pe-lg-5">%s</p>',
+      'subtitle_lead' => 'true',
 
       'paragraph' => 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare.</p><p class="mb-6">Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis.',
       'patternParagraph' => '<p>%s</p>',
@@ -67,10 +68,10 @@ $block = new CW_Settings(
             </div>
             <!--/column -->
             <div class="col-lg-6 <?php echo $block->column_class_2; ?>">
-               <?php echo $block->subtitle; ?>
-               <!--/subtitle -->
                <?php echo $block->title; ?>
                <!--/title -->
+               <?php echo $block->subtitle; ?>
+               <!--/subtitle -->
                <?php echo $block->paragraph; ?>
                <!--/pargraph -->
                <?php echo $block->buttons; ?>
