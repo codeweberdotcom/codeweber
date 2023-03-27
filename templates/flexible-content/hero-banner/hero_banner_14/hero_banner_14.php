2c0c5e5ab292f292cd4c1499ec57b3cc7f2a3e8e
Inna <inna@z-website.ru>
2023-03-27 16:41:49 +0300

    Hero 14 fix

--- templates/flexible-content/hero-banner/hero_banner_14/hero_banner_14.php
+++ templates/flexible-content/hero-banner/hero_banner_14/hero_banner_14.php
@@ -36,8 +36,8 @@ $block = new CW_Settings(
       'swiper' => array(
          'swiper_container_class' => 'rounded mb-md-n20',
          'image_class' => 'rounded mb-md-n20',
-         'wrapper_image_class' => NULL,
-         'image_pattern' => '<figure %5$s %9$s><img class="w-auto" src="%1$s" srcset="%2$s" alt="%3$s" />%7$s %10$s %11$s</figure>',
+         'wrapper_image_class' => 'rounded mb-md-n20',
+         'image_pattern' => '<figure %5$s %9$s><img src="%1$s" srcset="%2$s" alt="%3$s" />%7$s %10$s %11$s</figure>',
          'image_thumb_size' => 'sandbox_hero_14',
          'image_demo' => '<figure class="rounded mb-md-n20"><img src="' . get_template_directory_uri() . '/dist/img/photos/about18.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about18@2x.jpg 2x" alt="" /></figure>',
          'image_big_size' => 'project_1',
