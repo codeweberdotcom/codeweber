47a8ff065c89c36600070af69712281b7111af60
Denis Korablev <info@codeweber.com>
2023-03-28 01:27:54 +0300

    sidebar main

--- sidebar.php
+++ sidebar.php
@@ -13,38 +13,44 @@ if (class_exists('WooCommerce')) {
 }
 
 
-if (is_page() && !dynamic_sidebar()) : ?>
-	<?php if (is_active_sidebar('sidebar-page') || has_action('sidebar_page_start') || has_action('sidebar_page_end')) : ?>
+if (is_page() && !dynamic_sidebar()) { ?>
+	<?php if (is_active_sidebar('sidebar-page') || has_action('sidebar_page_start') || has_action('sidebar_page_end')) { ?>
 		<aside class="col-sm-4 mt-5 mt-md-0 ps-md-5">
 			<?php do_action('sidebar_page_start'); ?>
 			<?php dynamic_sidebar('sidebar-page'); ?>
 			<?php do_action('sidebar_page_end'); ?>
 		</aside> <!-- #sidebar-main-wrapper -->
-	<?php endif; ?>
+	<?php }; ?>
 
-<?php elseif (is_post_type_archive('services') || is_tax('service_category') || is_tax('types_of_services') || is_singular('services')) : ?>
-	<?php if (is_active_sidebar('sidebar-services') || has_action('sidebar_services_start') || has_action('sidebar_services_end')) : ?>
+<?php } elseif (is_post_type_archive('services') || is_tax('service_category') || is_tax('types_of_services') || is_singular('services')) { ?>
+	<?php if (is_active_sidebar('sidebar-services') || has_action('sidebar_services_start') || has_action('sidebar_services_end')) { ?>
 		<aside class="col-sm-4 mt-5 mt-md-0 ps-md-5">
 			<?php do_action('sidebar_services_start'); ?>
 			<?php dynamic_sidebar('sidebar-services'); ?>
 			<?php do_action('sidebar_services_end'); ?>
 		</aside> <!-- #sidebar-main-wrapper -->
-	<?php endif; ?>
+	<?php }; ?>
 
-<?php elseif (is_post_type_archive('faq') || is_singular('faq') || is_tax('faq_tag') || is_tax('faq_categories')) : ?>
-	<?php if (is_active_sidebar('sidebar_faq') || has_action('sidebar_faq_start') || has_action('sidebar_faq_end')) : ?>
+<?php } elseif (is_post_type_archive('faq') || is_singular('faq') || is_tax('faq_tag') || is_tax('faq_categories')) { ?>
+	<?php if (is_active_sidebar('sidebar_faq') || has_action('sidebar_faq_start') || has_action('sidebar_faq_end')) { ?>
 		<aside class="col-sm-4 mt-5 mt-md-0 ps-md-5">
 			<?php do_action('sidebar_faq_start'); ?>
 			<?php dynamic_sidebar('sidebar_faq'); ?>
 			<?php do_action('sidebar_faq_end'); ?>
 		</aside> <!-- #sidebar-main-wrapper -->
-	<?php endif; ?>
-<?php elseif (is_post_type_archive('testimonials')) :  ?>
-	<?php if (is_active_sidebar('sidebar-testimonials') || has_action('sidebar_testimonials_start') || has_action('sidebar_testimonials_end')) : ?>
+	<?php }; ?>
+<?php } elseif (is_post_type_archive('testimonials')) { ?>
+	<?php if (is_active_sidebar('sidebar-testimonials') || has_action('sidebar_testimonials_start') || has_action('sidebar_testimonials_end')) { ?>
 		<aside class="col-sm-4 mt-5 mt-md-0 ps-md-5">
 			<?php do_action('sidebar_testimonials_start'); ?>
 			<?php dynamic_sidebar('sidebar-testimonials'); ?>
 			<?php do_action('sidebar_testimonials_end'); ?>
 		</aside> <!-- #sidebar-main-wrapper -->
-<?php endif;
-endif;
+	<?php };
+} else { ?>
+	<aside class="col-sm-4 mt-5 mt-md-0 ps-md-5">
+		<?php do_action('sidebar_main_start'); ?>
+		<?php dynamic_sidebar('sidebar-main'); ?>
+		<?php do_action('sidebar_main_end'); ?>
+	</aside> <!-- #sidebar-main-wrapper -->
+<?php };
