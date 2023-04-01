a5062b8482a7253a9165a40d6a7f79e83caa9b0d
Denis Korablev <info@codeweber.com>
2023-04-01 13:58:34 +0300

    portfolio 9 fix title

--- templates/flexible-content/portfolio/portfolio_9/portfolio_9.php
+++ templates/flexible-content/portfolio/portfolio_9/portfolio_9.php
@@ -6,11 +6,13 @@
 
 $block = new CW_Settings(
    $cw_settings = array(
-      'subtitle' => 'My Selected Shots',
-      'patternSubtitle' => '<h2 class="display-5 mb-3">%s</h2>',
+      'subtitle' => 'Photography is my passion and I love to turn ideas into beautiful things.',
+      'patternSubtitle' => '<p class="lead fs-lg">%s</p>',
+      'subtitle_lead' => 'true',
 
-      'title' => 'Photography is my passion and I love to turn ideas into beautiful things.',
-      'patternTitle' => '<p class="lead fs-lg">%s</p>',
+
+      'title' => 'My Selected Shots',
+      'patternTitle' => '<h2 class="display-5 mb-3">%s</h2>',
       'background_class_default' => 'wrapper bg-light',
       'divider' => true,
    )
@@ -26,10 +28,10 @@ $terms = get_terms([
    <div class="container py-14 py-md-16 text-center">
       <div class="row">
          <div class="col-lg-10 col-xl-8 col-xxl-7 mx-auto mb-8">
-            <?php echo $block->subtitle; ?>
-            <!--/subtitle -->
             <?php echo $block->title; ?>
             <!--/title -->
+            <?php echo $block->subtitle; ?>
+            <!--/subtitle -->
          </div>
          <!-- /column -->
       </div>
