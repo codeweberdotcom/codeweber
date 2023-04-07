ecc6eafa62b2c00a24037d9ae3b0981f7f57fc67
Denis Korablev <info@codeweber.com>
2023-04-07 15:25:50 +0300

    Boottsrap navwalker

--- functions/lib/class-wp-bootstrap-navwalker.php
+++ functions/lib/class-wp-bootstrap-navwalker.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * WP Bootstrap Navwalker.
+ * WP Bootstrap Navwalker for Sandbox theme
  *
  * @package WP-Bootstrap-Navwalker
  *
  * @wordpress-plugin
- * Plugin Name: WP Bootstrap Navwalker
- * Plugin URI:  https://github.com/wp-bootstrap/wp-bootstrap-navwalker
+ * Plugin Name: WP Bootstrap Navwalker for Sandbox Theme
+ * Plugin URI:  https://github.com/codeweberdotcom/bootstrap_navwalker_5-_for_sandbox_theme.git
  * Description: A custom WordPress nav walker class to implement the Bootstrap 4 navigation style in a custom theme using the WordPress built in menu manager.
  * Author: Edward McIntyre - @twittem, WP Bootstrap, William Patton - @pattonwebz
  * Version: 4.3.0
- * Author URI: https://github.com/wp-bootstrap
- * GitHub Plugin URI: https://github.com/wp-bootstrap/wp-bootstrap-navwalker
+ * Author URI: https://codeweber.com
+ * GitHub Plugin URI: https://github.com/codeweberdotcom/bootstrap_navwalker_5-_for_sandbox_theme
  * GitHub Branch: master
  * License: GPL-3.0+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
