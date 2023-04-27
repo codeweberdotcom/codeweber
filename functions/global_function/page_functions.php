51d8ab4387721310ccf269ef606d293341f7afad
Denis Korablev <info@codeweber.com>
2023-04-27 13:46:09 +0300

    page header account

--- functions/global_function/page_functions.php
+++ functions/global_function/page_functions.php
@@ -418,7 +418,7 @@ function page_header()
       $codeweber['page_settings']['page_header_sub_title'] = NULL;
 
       //ready
-      if (get_field('page_header_type', $term_id_prefixed) && get_field('page_header_type', $term_id_prefixed) !== 'default' && get_field('page_header_type', $term_id_prefixed) !== 'disable') {
+      if (get_field('page_header_type', $term_id_prefixed) && get_field('page_header_type', $term_id_prefixed) !== 'default' && get_field('page_header_type', $term_id_prefixed) !== 'disable' && get_field('page_header_type', $term_id_prefixed) !== NULL) {
          $codeweber['page_settings']['page_header_type'] = get_field('page_header_type', $term_id_prefixed);
       } elseif (get_field('page_header_type', $term_id_prefixed) == 'default') {
          $codeweber['page_settings']['page_header_type'] = get_theme_mod('codeweber_page_header');
