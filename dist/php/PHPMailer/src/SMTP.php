83399aedcc2e3530d85a559cb4d97c9595e98d50
Denis Korablev <info@codeweber.com>
2024-04-07 23:24:46 +0300

    color atv

--- dist/php/PHPMailer/src/SMTP.php
+++ dist/php/PHPMailer/src/SMTP.php
@@ -83,28 +83,28 @@ class SMTP
      *
      * @var int
      */
-    const DEBUG_CLIENT = 0;
+    const DEBUG_CLIENT = 1;
 
     /**
      * Debug level to show client -> server and server -> client messages.
      *
      * @var int
      */
-    const DEBUG_SERVER = 0;
+    const DEBUG_SERVER = 2;
 
     /**
      * Debug level to show connection status, client -> server and server -> client messages.
      *
      * @var int
      */
-    const DEBUG_CONNECTION = 0;
+    const DEBUG_CONNECTION = 3;
 
     /**
      * Debug level to show all messages.
      *
      * @var int
      */
-    const DEBUG_LOWLEVEL = 0;
+    const DEBUG_LOWLEVEL = 4;
 
     /**
      * Debug output level.
@@ -289,7 +289,7 @@ class SMTP
                 $str = preg_replace('/\r\n|\r/m', "\n", $str);
                 echo gmdate('Y-m-d H:i:s'),
                 "\t",
-                //Trim trailing space
+                    //Trim trailing space
                 trim(
                     //Indent for readability, except for trailing break
                     str_replace(
@@ -329,7 +329,7 @@ class SMTP
         //Connect to the SMTP server
         $this->edebug(
             "Connection: opening to $host:$port, timeout=$timeout, options=" .
-                (count($options) > 0 ? var_export($options, true) : 'array()'),
+            (count($options) > 0 ? var_export($options, true) : 'array()'),
             self::DEBUG_CONNECTION
         );
 
@@ -420,7 +420,7 @@ class SMTP
             );
             $this->edebug(
                 'SMTP ERROR: ' . $this->error['error']
-                    . ": $errstr ($errno)",
+                . ": $errstr ($errno)",
                 self::DEBUG_CLIENT
             );
 
@@ -1009,7 +1009,7 @@ class SMTP
             //Cut off error code from each response line
             $detail = preg_replace(
                 "/{$code}[ -]" .
-                    ($code_ex ? str_replace('.', '\\.', $code_ex) . ' ' : '') . '/m',
+                ($code_ex ? str_replace('.', '\\.', $code_ex) . ' ' : '') . '/m',
                 '',
                 $this->last_reply
             );
@@ -1281,7 +1281,7 @@ class SMTP
             if ($endtime && time() > $endtime) {
                 $this->edebug(
                     'SMTP -> get_lines(): timelimit reached (' .
-                        $this->Timelimit . ' sec)',
+                    $this->Timelimit . ' sec)',
                     self::DEBUG_LOWLEVEL
                 );
                 break;
--- dist/php/contact.php
+++ dist/php/contact.php
@@ -4,91 +4,42 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-// require_once('../../../../../wp-load.php');
-
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 /*
 *  CONFIGURATION
 */
 
 // Recipients
-if (have_rows('recipients', 'option')) :
-  while (have_rows('recipients', 'option')) : the_row();
-    $fromEmail = get_sub_field('from_email');
-    $fromName = get_sub_field('from_name');
-    $sendToEmail = get_sub_field('send_to_email');
-    $sendToName = get_sub_field('send_to_name');
-  endwhile;
-endif;
-
-// Recipients
-// $fromEmail = 'info@codeweber.com'; // Email address that will be in the from field of the message.
-// $fromName = 'Codeweber'; // Name that will be in the from field of the message.
-// $sendToEmail = 'gigamarket24@yandex.ru'; // Email address that will receive the message with the output of the form
-// $sendToName = 'Вася'; // Name that will receive the message with the output of the form
-
-
-if (have_rows('settings', 'option')) :
-  while (have_rows('settings', 'option')) : the_row();
-    $subject = get_sub_field('subject');
-    $okMessage = get_sub_field('ok_message');
-    $errorMessage = get_sub_field('error_message');
-  endwhile;
-endif;
-
+$fromEmail = 'from@example.com'; // Email address that will be in the from field of the message.
+$fromName = 'From Name'; // Name that will be in the from field of the message.
+$sendToEmail = 'to@example.com'; // Email address that will receive the message with the output of the form
+$sendToName = 'To Name'; // Name that will receive the message with the output of the form
 
 // Subject
-// $subject = 'Message from Sandbox contact form';
+$subject = 'Message from Sandbox contact form';
 
 // Fields - Value of attribute name => Text to appear in the email
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message', 'department' => 'Department');
 
 // Success and error alerts
-// $okMessage = 'We have received your inquiry. Stay tuned, we’ll get back to you very soon.';
-// $errorMessage = 'There was an error while submitting the form. Please try again later';
-
-
-// SMTP settings
-if (have_rows('smtp', 'option')) :
-  while (have_rows('smtp', 'option')) : the_row();
-    $smtpUse = get_sub_field('smtp_use');
-    $smtpHost = get_sub_field('smtp_host');
-    $smtpUsername = get_sub_field('smtp_username');
-    $smtpPassword = get_sub_field('smtp_password');
-    $smtpSecure = get_sub_field('smtp_secure');
-    $smtpAutoTLS = get_sub_field('smtp_autotls');
-    $smtpPort = get_sub_field('smtp_port');
-  endwhile;
-endif;
-
+$okMessage = 'We have received your inquiry. Stay tuned, we’ll get back to you very soon.';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // SMTP settings
-// $smtpUse = false; // Set to true to enable SMTP authentication
-// $smtpHost = 'smtp@yandex.ru'; // Enter SMTP host ie. smtp.gmail.com
-// $smtpUsername = 'info@codeweber.com'; // SMTP username ie. gmail address
-// $smtpPassword = 'yGLX9gly!'; // SMTP password ie gmail password
-// $smtpSecure = 'tls'; // Enable TLS or SSL encryption
-// $smtpAutoTLS = true; // Enable Auto TLS
-// $smtpPort = 587; // TCP port to connect to
-
-
-// reCAPTCHA settings
-if (have_rows('recaptcha', 'option')) :
-  while (have_rows('recaptcha', 'option')) : the_row();
-    $recaptchaUse = get_sub_field('recaptcha_use');
-    $recaptchaSecret = get_sub_field('recaptcha_secret');
-  endwhile;
-endif;
-
+$smtpUse = false; // Set to true to enable SMTP authentication
+$smtpHost = ''; // Enter SMTP host ie. smtp.gmail.com
+$smtpUsername = ''; // SMTP username ie. gmail address
+$smtpPassword = ''; // SMTP password ie gmail password
+$smtpSecure = 'tls'; // Enable TLS or SSL encryption
+$smtpAutoTLS = false; // Enable Auto TLS
+$smtpPort = 587; // TCP port to connect to
 
 // reCAPTCHA settings
-// $recaptchaUse = false; // Set to true to enable reCAPTHCA
-// $recaptchaSecret = 'YOUR_SECRET_KEY'; // enter your secret key from https://www.google.com/recaptcha/admin
-
+$recaptchaUse = false; // Set to true to enable reCAPTHCA
+$recaptchaSecret = 'YOUR_SECRET_KEY'; // enter your secret key from https://www.google.com/recaptcha/admin
 
 /*
 *  LET'S DO THE SENDING
@@ -97,8 +48,8 @@ endif;
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 try {
-  if (count($_POST) == 0) throw new \Exception('Form is empty');
-  if ($recaptchaUse == true) {
+  if(count($_POST) == 0) throw new \Exception('Form is empty');
+  if($recaptchaUse == true) {
     require('recaptcha/src/autoload.php');
     if (!isset($_POST['g-recaptcha-response'])) {
       throw new \Exception('ReCaptcha is not set.');
@@ -110,7 +61,6 @@ try {
       throw new \Exception('ReCaptcha was not validated.');
     }
   }
-  $emailTextHtml = '';
   $emailTextHtml .= "<table>";
   foreach ($_POST as $key => $value) {
     // If the field exists in the $fields array, include it in the email
@@ -118,8 +68,6 @@ try {
       $emailTextHtml .= "<tr><th><b>$fields[$key]</b></th><td>$value</td></tr>";
     }
   }
-  $from = $fromEmail;
-
   $emailTextHtml .= "</table>";
   $mail = new PHPMailer;
   $mail->setFrom($fromEmail, $fromName);
@@ -130,7 +78,7 @@ try {
   $mail->Subject = $subject;
   $mail->Body    = $emailTextHtml;
   $mail->msgHTML($emailTextHtml);
-  if ($smtpUse == true) {
+  if($smtpUse == true) {
     // Tell PHPMailer to use SMTP
     $mail->isSMTP();
     // Enable SMTP debugging
@@ -138,9 +86,9 @@ try {
     // 1 = client messages
     // 2 = client and server messages
     $mail->Debugoutput = function ($str, $level) use (&$mailerErrors) {
-      $mailerErrors[] = ['str' => $str, 'level' => $level];
+      $mailerErrors[] = [ 'str' => $str, 'level' => $level ];
     };
-    $mail->SMTPDebug = 0;
+    $mail->SMTPDebug = 3;
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = $smtpSecure;
     $mail->SMTPAutoTLS = $smtpAutoTLS;
@@ -149,16 +97,17 @@ try {
     $mail->Username = $smtpUsername;
     $mail->Password = $smtpPassword;
   }
-  if (!$mail->send()) {
+  if(!$mail->send()) {
     throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
   }
   $responseArray = array('type' => 'success', 'message' => $okMessage);
-} catch (\Exception $e) {
+}
+catch (\Exception $e) {
   $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
 }
 // if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-  $encoded = json_encode($responseArray);
+  $encoded = json_encode($responseArray); 
   header('Content-Type: application/json');
   echo $encoded;
 }
