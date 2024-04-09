b08406f46bc52fa5f47ecd0f0c26321dcf909018
Denis Korablev <info@codeweber.com>
2024-04-09 14:25:17 +0300

    footer functions

--- functions/global_function/footer_functions.php
+++ functions/global_function/footer_functions.php
@@ -27,9 +27,21 @@ function cta_footer_text()
 function footer_about_company_option()
 {
    if (get_field('about_company', 'option')) {
-      $about_company_option = '<p class="lead mb-0">' . get_field('about_company', 'option') . '</p>';
+      $about_company_option = '<p class="mb-0">' . get_field('about_company', 'option') . '</p>';
    } else {
       $about_company_option = NULL;
    }
    return $about_company_option;
+}
+
+
+/* Footer Year */
+function year_of_birth_company()
+{
+   if (get_field('year_of_birth_of_the_company', 'option')) {
+      $year_of_bth_comp = '© ' . get_field('year_of_birth_of_the_company', 'option') . '-' . date("Y") . ' ' . esc_html__('Made with', 'codeweber') . ' ' . esc_attr(wp_get_theme()->get('Author'));
+   } else {
+      $year_of_bth_comp = '© ' . date("Y") . ' ' . esc_html__('Made with', 'codeweber') . ' ' . esc_attr(wp_get_theme()->get('Author'));
+   }
+   return $year_of_bth_comp;
 }
